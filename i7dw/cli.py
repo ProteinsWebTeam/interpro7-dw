e3419cb84c3207b5af733733c4a2f6af40fb6341
Matthias Blum <mat.blum@gmail.com>
2018-09-18 17:33:15 +0100

    Use lastest version of mundone

--- i7dw/cli.py
+++ i7dw/cli.py
@@ -4,6 +4,7 @@
 import argparse
 import configparser
 import os
+import sys
 
 from mundone import Task, Workflow
 
@@ -33,19 +34,35 @@ def parse_elastic_hosts(str_hosts):
 
 
 def cli():
-    parser = argparse.ArgumentParser(description='Build InterPro7 data warehouse')
-    parser.add_argument('config', metavar='config.ini', help='configuration file')
-    parser.add_argument('-t', '--tasks', nargs='*', help='tasks to run')
-    parser.add_argument('-l', '--list', action='store_true', default=False,
-                        help='list steps that would be processed, but do not process them')
-    parser.add_argument('--nodep', action='store_true', default=False,
-                        help='do not include dependencies (run only the requested tasks)')
-    parser.add_argument('-d', '--detach', action='store_true', default=False,
-                        help='do not wait for tasks to complete (only tasks without dependencies are run)')
+    parser = argparse.ArgumentParser(
+        description="Build InterPro7 data warehouse"
+    )
+    parser.add_argument("config",
+                        metavar="config.ini",
+                        help="configuration file")
+    parser.add_argument("-t", "--tasks",
+                        nargs="*",
+                        help="tasks to run")
+    parser.add_argument("--dry-run",
+                        action="store_true",
+                        default=False,
+                        help="list tasks to run and quit")
+    parser.add_argument("--resume",
+                        action="store_true",
+                        default=False,
+                        help="skip completed tasks")
+    parser.add_argument("--detach",
+                        action="store_true",
+                        default=False,
+                        help="collect completed tasks, "
+                             "submit tasks ready, and quit")
     args = parser.parse_args()
 
     if not os.path.isfile(args.config):
-        raise RuntimeError("cannot open '{}': no such file or directory".format(args.config))
+        raise RuntimeError(
+            "cannot open '{}': "
+            "no such file or directory".format(args.config)
+        )
 
     config = configparser.ConfigParser()
     config.read(args.config)
@@ -54,263 +71,259 @@ def cli():
     if not os.path.isdir(export_dir):
         os.makedirs(export_dir)
 
-    interpro_oracle = config['databases']['interpro_oracle']
-    interpro_mysql = config['databases']['interpro_mysql']
-    pfam_mysql = config['databases']['pfam_mysql']
+    ora_ipro = config['databases']['interpro_oracle']
+    my_ipro = config['databases']['interpro_mysql']
+    my_pfam = config['databases']['pfam_mysql']
+    queue = config['workflow']['queue']
+
+    elastic_hosts = config['elastic']['hosts'].split(',')
+    elastic_dir = config['elastic']['dir']
+
+    threshold = config.getfloat("jaccard", "threshold")
 
     tasks = [
+        # Export data to stores
         Task(
-            name='export_comments',
+            name="export_comments",
             fn=uniprot.export_protein_comments,
-            args=(interpro_oracle, os.path.join(export_dir, 'comments.bs')),
+            args=(ora_ipro, os.path.join(export_dir, "comments.bs")),
             kwargs=dict(chunk_size=100000),
-            lsf=dict(queue=config['workflow']['queue'], mem=1000)
+            scheduler=dict(queue=queue, mem=1000)
         ),
         Task(
-            name='export_descriptions',
+            name="export_descriptions",
             fn=uniprot.export_protein_descriptions,
-            args=(interpro_oracle, os.path.join(export_dir, 'descriptions.bs')),
+            args=(ora_ipro, os.path.join(export_dir, 'descriptions.bs')),
             kwargs=dict(chunk_size=100000),
-            lsf=dict(queue=config['workflow']['queue'], mem=1000)
+            scheduler=dict(queue=queue, mem=1000)
         ),
         Task(
-            name='export_evidences',
+            name="export_evidences",
             fn=uniprot.export_protein_evidence,
-            args=(interpro_oracle, os.path.join(export_dir, 'evidences.bs')),
+            args=(ora_ipro, os.path.join(export_dir, 'evidences.bs')),
             kwargs=dict(chunk_size=100000),
-            lsf=dict(queue=config['workflow']['queue'], mem=500)
+            scheduler=dict(queue=queue, mem=500)
         ),
         Task(
-            name='export_genes',
+            name="export_gene",
             fn=uniprot.export_protein_gene,
-            args=(interpro_oracle, os.path.join(export_dir, 'genes.bs')),
+            args=(ora_ipro, os.path.join(export_dir, 'genes.bs')),
             kwargs=dict(chunk_size=100000),
-            lsf=dict(queue=config['workflow']['queue'], mem=500)
+            scheduler=dict(queue=queue, mem=500)
         ),
         Task(
-            name='export_proteomes',
+            name="export_proteomes",
             fn=uniprot.export_protein_proteomes,
-            args=(interpro_oracle, os.path.join(export_dir, 'proteomes.bs')),
+            args=(ora_ipro, os.path.join(export_dir, 'proteomes.bs')),
             kwargs=dict(chunk_size=100000),
-            lsf=dict(queue=config['workflow']['queue'], mem=500)
+            scheduler=dict(queue=queue, mem=500)
         ),
         Task(
-            name='export_annotations',
+            name="export_annotations",
             fn=goa.export_annotations,
-            args=(interpro_oracle, os.path.join(export_dir, 'annotations.bs')),
+            args=(ora_ipro, os.path.join(export_dir, 'annotations.bs')),
             kwargs=dict(chunk_size=100000),
-            lsf=dict(queue=config['workflow']['queue'], mem=2000)
+            scheduler=dict(queue=queue, mem=2000)
         ),
         Task(
-            name='export_struct_matches',
+            name="export_structures",
             fn=interpro.export_struct_matches,
-            args=(interpro_oracle, os.path.join(export_dir, 'struct_matches.bs')),
+            args=(ora_ipro, os.path.join(export_dir, 'struct_matches.bs')),
             kwargs=dict(chunk_size=100000),
-            lsf=dict(queue=config['workflow']['queue'], mem=2000)
+            scheduler=dict(queue=queue, mem=2000)
         ),
         Task(
-            name='export_prot_matches',
+            name="export_matches",
             fn=interpro.export_prot_matches,
-            args=(interpro_oracle, os.path.join(export_dir, 'prot_matches.bs')),
+            args=(ora_ipro, os.path.join(export_dir, 'prot_matches.bs')),
             kwargs=dict(chunk_size=100000),
-            lsf=dict(queue=config['workflow']['queue'], mem=3000)
+            scheduler=dict(queue=queue, mem=3000)
         ),
         Task(
-            name='export_prot_matches_extra',
+            name="export_features",
             fn=interpro.export_prot_matches_extra,
-            args=(interpro_oracle, os.path.join(export_dir, 'prot_matches_extra.bs')),
+            args=(ora_ipro, os.path.join(export_dir, 'prot_matches_extra.bs')),
             kwargs=dict(chunk_size=100000),
-            lsf=dict(queue=config['workflow']['queue'], mem=2000)
+            scheduler=dict(queue=queue, mem=2000)
         ),
         Task(
-            name='export_residues',
             fn=interpro.export_residues,
-            args=(interpro_oracle, os.path.join(export_dir, 'residues.bs')),
+            args=(ora_ipro, os.path.join(export_dir, 'residues.bs')),
             kwargs=dict(chunk_size=100000),
-            lsf=dict(queue=config['workflow']['queue'], mem=3000)
+            scheduler=dict(queue=queue, mem=3000)
         ),
         Task(
-            name='export_proteins',
             fn=interpro.export_proteins,
-            args=(interpro_oracle, os.path.join(export_dir, 'proteins.bs')),
+            args=(ora_ipro, os.path.join(export_dir, 'proteins.bs')),
             kwargs=dict(chunk_size=100000),
-            lsf=dict(queue=config['workflow']['queue'], mem=2000)
+            scheduler=dict(queue=queue, mem=2000)
         ),
 
+        # Load data to MySQL
         Task(
-            name='init_mysql',
-            fn=mysql.init,
-            args=(interpro_mysql,),
-            lsf=dict(queue=config['workflow']['queue'])
+            fn=mysql.init_tables,
+            args=(my_ipro,),
+            scheduler=dict(queue=queue)
         ),
         Task(
-            name='insert_taxa',
             fn=mysql.insert_taxa,
-            args=(interpro_oracle, interpro_mysql),
-            lsf=dict(queue=config['workflow']['queue'], mem=4000),
-            requires=['init_mysql'],
+            args=(ora_ipro, my_ipro),
+            scheduler=dict(queue=queue, mem=4000),
+            requires=["init_tables"]
         ),
         Task(
-            name='insert_proteomes',
             fn=mysql.insert_proteomes,
-            args=(interpro_oracle, interpro_mysql),
-            lsf=dict(queue=config['workflow']['queue'], mem=1000),
-            requires=['insert_taxa'],
+            args=(ora_ipro, my_ipro),
+            scheduler=dict(queue=queue, mem=1000),
+            requires=["insert_taxa"]
         ),
-
         Task(
-            name='insert_databases',
             fn=mysql.insert_databases,
-            args=(interpro_oracle, interpro_mysql),
-            lsf=dict(queue=config['workflow']['queue']),
-            requires=['init_mysql'],
+            args=(ora_ipro, my_ipro),
+            scheduler=dict(queue=queue),
+            requires=["init_tables"]
         ),
-
         Task(
-            name='insert_entries',
             fn=mysql.insert_entries,
-            args=(interpro_oracle, pfam_mysql, interpro_mysql),
-            lsf=dict(queue=config['workflow']['queue'], mem=16000),
-            requires=['insert_databases'],
+            args=(ora_ipro, my_pfam, my_ipro),
+            scheduler=dict(queue=queue, mem=2000),
+            requires=["insert_databases"]
         ),
         Task(
-            name='insert_annotations',
             fn=mysql.insert_annotations,
-            args=(pfam_mysql, interpro_mysql),
-            lsf=dict(queue=config['workflow']['queue'], mem=16000),
-            requires=['insert_entries'],
+            args=(my_pfam, my_ipro),
+            scheduler=dict(queue=queue, mem=4000),
+            requires=["insert_entries"]
         ),
-
         Task(
-            name='insert_structures',
             fn=mysql.insert_structures,
-            args=(interpro_oracle, interpro_mysql),
-            lsf=dict(queue=config['workflow']['queue'], mem=16000),
-            requires=['insert_databases'],
+            args=(ora_ipro, my_ipro),
+            scheduler=dict(queue=queue, mem=1000),
+            requires=["insert_databases"]
         ),
-
         Task(
-            name='insert_sets',
             fn=mysql.insert_sets,
-            args=(pfam_mysql, interpro_mysql),
-            lsf=dict(queue=config['workflow']['queue']),
-            requires=['insert_databases'],
+            args=(my_pfam, my_ipro),
+            scheduler=dict(queue=queue),
+            requires=["insert_databases"]
         ),
-
         Task(
-            name='insert_proteins',
             fn=mysql.insert_proteins,
             args=(
-                interpro_mysql,
-                os.path.join(export_dir, 'proteins.bs'),
-                os.path.join(export_dir, 'evidences.bs'),
-                os.path.join(export_dir, 'descriptions.bs'),
-                os.path.join(export_dir, 'comments.bs'),
-                os.path.join(export_dir, 'proteomes.bs'),
-                os.path.join(export_dir, 'genes.bs'),
-                os.path.join(export_dir, 'annotations.bs'),
-                os.path.join(export_dir, 'residues.bs'),
-                os.path.join(export_dir, 'struct_matches.bs'),
-                os.path.join(export_dir, 'prot_matches_extra.bs')
+                my_ipro,
+                os.path.join(export_dir, "proteins.bs"),
+                os.path.join(export_dir, "evidences.bs"),
+                os.path.join(export_dir, "descriptions.bs"),
+                os.path.join(export_dir, "comments.bs"),
+                os.path.join(export_dir, "proteomes.bs"),
+                os.path.join(export_dir, "genes.bs"),
+                os.path.join(export_dir, "annotations.bs"),
+                os.path.join(export_dir, "residues.bs"),
+                os.path.join(export_dir, "struct_matches.bs"),
+                os.path.join(export_dir, "prot_matches_extra.bs")
             ),
-            lsf=dict(queue=config['workflow']['queue'], mem=64000),
+            scheduler=dict(queue=queue, mem=32000),
             requires=[
-                'insert_taxa', 'export_proteins', 'export_evidences', 'export_descriptions', 'export_comments',
-                'export_proteomes', 'export_genes', 'export_annotations', 'export_residues', 'export_struct_matches',
-                'export_prot_matches_extra'
-            ],
+                "insert_taxa", "export_proteins", "export_evidences",
+                "export_descriptions", "export_comments",
+                "export_proteomes", "export_genes",
+                "export_annotations", "export_residues",
+                "export_structures", "export_features"
+            ]
         ),
 
+        # Create EBI Search index
         Task(
-            name='index_relationships',
-            fn=elastic.index_relationships,
-            args=(
-                interpro_oracle,
-                interpro_mysql,
-                os.path.join(export_dir, 'proteins.bs'),
-                os.path.join(export_dir, 'descriptions.bs'),
-                os.path.join(export_dir, 'comments.bs'),
-                os.path.join(export_dir, 'proteomes.bs'),
-                os.path.join(export_dir, 'prot_matches.bs'),
-                config['elastic']['dir']
-            ),
-            kwargs=dict(
-                producers=3,
-                loaders=4,
-                supermatches=True,
-                hosts=parse_elastic_hosts(config['elastic']['hosts']),
-                doc_type=config['elastic']['type'],
-                suffix=config['meta']['release'],
-                indices_json=config['elastic']['indices'],
-                properties_json=config['elastic']['properties'],
-                shards=config.getint('elastic', 'shards')
-            ),
-            lsf=dict(queue=config['workflow']['queue'], mem=96000, cpu=9),
-            requires=[
-                'insert_entries', 'insert_sets', 'insert_taxa',
-                'export_proteins', 'export_descriptions', 'export_comments', 'export_proteomes', 'export_prot_matches'
-            ],
-        ),
-
-        Task(
-            name='collect',
-            fn=elastic.collect,
-            args=(
-                interpro_mysql,
-                parse_elastic_hosts(config['elastic']['hosts']),
-                config['elastic']['type'],
-                config['elastic']['dir']
-            ),
-            kwargs=dict(
-                suffix=config['meta']['release'],
-                processes=4,
-                gzip=True
-            ),
-            lsf=dict(queue=config['workflow']['queue'], mem=16000, cpu=5),
-            requires=['index_relationships'],
-        ),
-
-        Task(
-            name='update_alias',
-            fn=elastic.update_alias,
-            args=(
-                interpro_mysql,
-                parse_elastic_hosts(config['elastic']['hosts']),
-                config['elastic']['alias'],
-                config['meta']['release']
-            ),
-            lsf=dict(queue=config['workflow']['queue']),
-            requires=['collect'],
-        ),
-
-        Task(
-            name='ebisearch',
             fn=ebisearch.export,
             args=(
-                interpro_mysql,
-                os.path.join(export_dir, 'proteins.bs'),
+                my_ipro,
+                os.path.join(export_dir, "proteins.bs"),
                 os.path.join(export_dir, 'prot_matches.bs'),
-                os.path.join(export_dir, 'struct_matches.bs'),
-                os.path.join(export_dir, 'proteomes.bs'),
-                config['meta']['name'],
-                config['meta']['release'],
-                config['meta']['release_date'],
-                config['ebisearch']['dir'],
+                os.path.join(export_dir, "struct_matches.bs"),
+                os.path.join(export_dir, "proteomes.bs"),
+                config["meta"]["name"],
+                config["meta"]["release"],
+                config["meta"]["release_date"],
+                config["ebisearch"]["dir"]
             ),
-            lsf=dict(queue=config['workflow']['queue'], mem=24000, tmp=10000),
+            scheduler=dict(
+                queue=config['workflow']['queue'], mem=24000, tmp=10000
+            ),
             requires=[
-                'insert_entries', 'export_proteins', 'export_prot_matches', 'export_struct_matches',
-                'export_proteomes',
+                "insert_entries", "export_proteins", "export_matches",
+                "export_structures", "export_proteomes",
             ],
         ),
+
+        # Indexing Elastic documents
+        Task(
+            fn=elastic.init_dir,
+            args=(elastic_dir,),
+            scheduler=dict(queue=queue),
+        ),
+        Task(
+            fn=elastic.create_documents,
+            args=(
+                ora_ipro,
+                my_ipro,
+                os.path.join(export_dir, "proteins.bs"),
+                os.path.join(export_dir, "descriptions.bs"),
+                os.path.join(export_dir, "comments.bs"),
+                os.path.join(export_dir, "proteomes.bs"),
+                os.path.join(export_dir, 'prot_matches.bs'),
+                elastic_dir
+            ),
+            kwargs=dict(producers=3, threshold=threshold),
+            scheduler=dict(queue=queue, cpu=4, mem=64000),
+            requires=(
+                "insert_entries", "insert_sets", "insert_taxa",
+                "export_proteins", "export_descriptions",
+                "export_comments", "export_proteomes",
+                "export_matches", "init_dir"
+            )
+        )
     ]
 
-    if args.detach:
-        secs = 0
-        cascade_kill = False
-    else:
-        secs = 10
-        cascade_kill = True
+    for host in elastic_hosts:
+        tasks.append(
+            Task(
+                fn=elastic.index_documents,
+                args=(my_ipro, host, config['elastic']['type'], config['elastic']['properties'], elastic_dir),
+                kwargs=dict(
+                    indices=config['elastic']['indices'],
+                    suffix=config['meta']['release'],
+                    loaders=4
+                ),
+                scheduler=dict(queue=queue, cpu=5, mem=16000),
+                requires=("init_dir", "insert_databases")
+            )
+        )
 
-    w = Workflow(tasks, dir=config['workflow']['dir'], db=config['workflow']['db'], cascade_kill=cascade_kill)
-    w.run(args.tasks, process=(not args.list), incdep=(not args.nodep), secs=secs)
+    task_names = []
+    for t in tasks:
+        if t.name not in task_names:
+            task_names.append(t.name)
+
+    if args.tasks:
+        for arg in args.tasks:
+            if arg not in task_names:
+                sys.stderr.write(
+                    "{}: error: "
+                    "argument -t/--tasks: "
+                    "invalid choice: '{}' (choose from {})\n".format(
+                        os.path.basename(sys.argv[0]),
+                        arg,
+                        ', '.join(map("'{}'".format, task_names))
+                    )
+                )
+                exit(1)
+    else:
+        args.tasks = []
+
+    w = Workflow(tasks, name="InterPro7 DW", dir=config["workflow"]["dir"])
+    w.run(
+        args.tasks,
+        secs=0 if args.detach else 60,
+        resume=args.resume,
+        dry=args.dry_run
+    )
