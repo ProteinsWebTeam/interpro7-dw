722d7ab4fb079064a7661057ec968d947c55b4b5
Matthias Blum <mat.blum@gmail.com>
2018-10-15 12:52:37 +0100

    Moving cross-reference counter to its own module

---
+++ i7dw/xref.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+import os
+import time
+from multiprocessing import Process, Queue
+from tempfile import mkdtemp
+
+from . import disk
+from .ebi import pdbe
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s: %(levelname)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+
+def feed_store(filepath, queue, **kwargs):
+    store = disk.KVStore(filepath, **kwargs)
+
+    while True:
+        chunks = queue.get()
+        if chunks is None:
+            break
+
+        for key, _type, value in chunks:
+            store.add(key, _type, value)
+
+        store.flush()
+
+    store.build()
+
+
+def count_xrefs(ora_uri, my_uri, proteins_f, prot_matches_f, proteomes_f,
+                entries_kvf, taxa_kvf, proteomes_kvf, sets_kvf,
+                structures_kvf, compress=False, tmpdir=None, chunk_size=100000):
+
+    try:
+        os.makedirs(tmpdir, exist_ok=True)
+    except (FileNotFoundError, TypeError):
+        tmpdir = None
+
+    l_entries = []
+    d_entries = mkdtemp(prefix="entries_", dir=tmpdir)
+    q_entries = Queue(maxsize=1)
+    p_entries = Process(target=feed_store,
+                        args=(entries_kvf, q_entries),
+                        kwargs={"compress": compress,
+                                "tmpdir": d_entries,
+                                "delete": False})
+
+    l_taxa = []
+    d_taxa = mkdtemp(prefix="taxa_", dir=tmpdir)
+    q_taxa = Queue(maxsize=1)
+    p_taxa = Process(target=feed_store,
+                     args=(taxa_kvf, q_entries),
+                     kwargs={"compress": compress,
+                             "tmpdir": d_taxa,
+                             "delete": False})
+
+    l_proteomes = []
+    d_proteomes = mkdtemp(prefix="proteomes_", dir=tmpdir)
+    q_proteomes = Queue(maxsize=1)
+    p_proteomes = Process(target=feed_store,
+                          args=(proteomes_kvf, q_entries),
+                          kwargs={"compress": compress,
+                                  "tmpdir": d_proteomes,
+                                  "delete": False})
+
+    l_sets = []
+    d_sets = mkdtemp(prefix="sets_", dir=tmpdir)
+    q_sets = Queue(maxsize=1)
+    p_sets = Process(target=feed_store,
+                     args=(sets_kvf, q_entries),
+                     kwargs={"compress": compress,
+                             "tmpdir": d_sets,
+                             "delete": False})
+
+    l_structures = []
+    d_structures = mkdtemp(prefix="structures_", dir=tmpdir)
+    q_structures = Queue(maxsize=1)
+    p_structures = Process(target=feed_store,
+                           args=(structures_kvf, q_entries),
+                           kwargs={"compress": compress,
+                                   "tmpdir": d_structures,
+                                   "delete": False})
+
+    for p in (p_entries, p_taxa, p_proteomes, p_sets, p_structures):
+        p.start()
+
+    structures = pdbe.get_structures(ora_uri, citations=False,
+                                     fragments=False, by_protein=True)
+
+    proteins_s = disk.Store(proteins_f)
+    prot_matches_s = disk.Store(prot_matches_f)
+    proteomes_s = disk.Store(proteomes_f)
+
+    n_proteins = 0
+    ts = time.time()
+    for acc, protein in proteins_s.iter():
+        tax_id = protein["taxon"]
+        matches = prot_matches_s.get(acc, [])
+        proteomes = proteomes_s.get(acc, [])
+        _structures = structures.get(acc, [])
+
+        l_taxa.append((tax_id, "protein", acc))
+
+        entries = set()
+        for m in matches:
+            entries.add(m["method_ac"])
+            if m["entry_ac"]:
+                entries.add(m["entry_ac"])
+
+        for pdbe_id in _structures:
+            l_structures.append((pdbe_id, "protein", acc))
+
+            for entry_ac in entries:
+                l_structures.append((pdbe_id, "entry", entry_ac))
+                l_entries.append((entry_ac, "structure", pdbe_id))
+
+        for upid in proteomes:
+            l_proteomes.append((upid, "protein", acc))
+
+            for entry_ac in entries:
+                l_proteomes.append((upid, "entry", entry_ac))
+                l_entries.append((entry_ac, "proteome", upid))
+
+        for entry_ac in entries:
+            l_entries.append((entry_ac, "protein", acc))
+            l_entries.append((entry_ac, "taxon", tax_id))
+            l_taxa.append((tax_id, "entry", entry_ac))
+
+        n_proteins += 1
+
+        if not n_proteins % chunk_size:
+            q_entries.put(l_entries)
+            l_entries = []
+            q_taxa.put(l_taxa)
+            l_taxa = []
+            q_proteomes.put(l_proteomes)
+            l_proteomes = []
+            q_sets.put(l_sets)
+            l_sets = []
+            q_structures.put(l_structures)
+            l_structures = []
+
+        if not n_proteins % 1000000:
+            logging.info('{:>12} ({:.0f} proteins/sec)'.format(
+                n_proteins, n_proteins // (time.time() - ts)
+            ))
+
+    logging.info('{:>12} ({:.0f} proteins/sec)'.format(
+        n_proteins, n_proteins // (time.time() - ts)
+    ))
+
+    proteins_s.close()
+    prot_matches_s.close()
+    proteomes_s.close()
+
+    for q in (q_entries, q_taxa, q_proteomes,  q_sets, q_structures):
+        q.put(None)
+
+
+    logging.info("complete")
