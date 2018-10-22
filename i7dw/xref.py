d7fb2d815e720d2158d730b98339cff6557396a9
Matthias Blum <mat.blum@gmail.com>
2018-10-22 13:28:23 +0100

    Pass IDs to stores to speed up bucketing

--- i7dw/xref.py
+++ i7dw/xref.py
@@ -2,13 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import multiprocessing as mp
 import os
 import time
-from multiprocessing import Process, Queue
-from tempfile import mkdtemp
 
 from . import disk, mysql
-from .ebi import pdbe
 
 logging.basicConfig(
     level=logging.INFO,
@@ -17,8 +15,7 @@ logging.basicConfig(
 )
 
 
-def feed_store(filepath: str, queue: Queue, **kwargs):
-
+def feed_store(filepath: str, queue: mp.Queue, **kwargs: dict):
     with disk.KVStore(filepath, **kwargs) as store:
         while True:
             chunks = queue.get()
@@ -32,74 +29,89 @@ def feed_store(filepath: str, queue: Queue, **kwargs):
 
         logging.info("{} filled".format(filepath))
 
-    logging.info("{} ready".format(filepath))
+        tmp_usage = store.close()
+
+    logging.info("{} ready (temporary files: {} bytes)".format(
+        filepath, tmp_usage
+    ))
 
 
-def count_xrefs(ora_uri, my_uri, proteins_f, prot_matches_f, proteomes_f,
+def count_xrefs(my_uri, proteins_f, prot_matches_f, proteomes_f,
                 entries_kvf, taxa_kvf, proteomes_kvf, sets_kvf,
                 structures_kvf, chunk_size=100000, compress=False,
-                delete=True, tmpdir=None):
+                tmpdir=None):
 
     try:
         os.makedirs(tmpdir, exist_ok=True)
     except (FileNotFoundError, TypeError):
         tmpdir = None
 
-    entries = {
+    entry2db = {
         acc: e["database"]
         for acc, e
         in mysql.get_entries(my_uri).items()
     }
+    entries = sorted(entry2db)
+    taxa = sorted(mysql.get_taxa(my_uri, slim=True))
+    proteomes = sorted(mysql.get_proteomes(my_uri))
+    sets = sorted(mysql.get_sets(my_uri, by_members=False))
+    structures = mysql.get_structures(my_uri)
+
+    prot2struct = {}
+    for s in structures.values():
+        for p in s["proteins"]:
+            if p in prot2struct:
+                prot2struct[p].add(s["accession"])
+            else:
+                prot2struct[p] = {s["accession"]}
+
+    structures = sorted(structures)
+
+    ctx = mp.get_context("spawn")
 
     l_entries = []
-    d_entries = mkdtemp(prefix="entries_", dir=tmpdir)
-    q_entries = Queue(maxsize=1)
-    p_entries = Process(target=feed_store,
-                        args=(entries_kvf, q_entries),
-                        kwargs={"compress": compress,
-                                "tmpdir": d_entries,
-                                "delete": delete})
+    q_entries = ctx.Queue(maxsize=1)
+    p_entries = ctx.Process(target=feed_store,
+                            args=(entries_kvf, q_entries),
+                            kwargs={"compress": compress,
+                                    "ids": entries,
+                                    "tmpdir": tmpdir})
 
     l_taxa = []
-    d_taxa = mkdtemp(prefix="taxa_", dir=tmpdir)
-    q_taxa = Queue(maxsize=1)
-    p_taxa = Process(target=feed_store,
-                     args=(taxa_kvf, q_taxa),
-                     kwargs={"compress": compress,
-                             "tmpdir": d_taxa,
-                             "delete": delete})
+    q_taxa = ctx.Queue(maxsize=1)
+    p_taxa = ctx.Process(target=feed_store,
+                         args=(taxa_kvf, q_taxa),
+                         kwargs={"compress": compress,
+                                 "ids": taxa,
+                                 "tmpdir": tmpdir})
 
     l_proteomes = []
-    d_proteomes = mkdtemp(prefix="proteomes_", dir=tmpdir)
-    q_proteomes = Queue(maxsize=1)
-    p_proteomes = Process(target=feed_store,
-                          args=(proteomes_kvf, q_proteomes),
-                          kwargs={"compress": compress,
-                                  "tmpdir": d_proteomes,
-                                  "delete": delete})
+    q_proteomes = ctx.Queue(maxsize=1)
+    p_proteomes = ctx.Process(target=feed_store,
+                              args=(proteomes_kvf, q_proteomes),
+                              kwargs={"compress": compress,
+                                      "ids": proteomes,
+                                      "tmpdir": tmpdir})
 
     l_sets = []
-    d_sets = mkdtemp(prefix="sets_", dir=tmpdir)
-    q_sets = Queue(maxsize=1)
-    p_sets = Process(target=feed_store,
-                     args=(sets_kvf, q_sets),
-                     kwargs={"compress": compress,
-                             "tmpdir": d_sets,
-                             "delete": delete})
+    q_sets = ctx.Queue(maxsize=1)
+    p_sets = ctx.Process(target=feed_store,
+                         args=(sets_kvf, q_sets),
+                         kwargs={"compress": compress,
+                                 "ids": sets,
+                                 "tmpdir": tmpdir})
 
     l_structures = []
-    d_structures = mkdtemp(prefix="structures_", dir=tmpdir)
-    q_structures = Queue(maxsize=1)
-    p_structures = Process(target=feed_store,
-                           args=(structures_kvf, q_structures),
-                           kwargs={"compress": compress,
-                                   "tmpdir": d_structures,
-                                   "delete": delete})
+    q_structures = ctx.Queue(maxsize=1)
+    p_structures = ctx.Process(target=feed_store,
+                               args=(structures_kvf, q_structures),
+                               kwargs={"compress": compress,
+                                       "ids": structures,
+                                       "tmpdir": tmpdir})
 
     for p in (p_entries, p_taxa, p_proteomes, p_sets, p_structures):
         p.start()
 
-    structures = pdbe.group_by_proteins(pdbe.get_structures(ora_uri))
     proteins_s = disk.Store(proteins_f)
     prot_matches_s = disk.Store(prot_matches_f)
     proteomes_s = disk.Store(proteomes_f)
@@ -110,7 +122,7 @@ def count_xrefs(ora_uri, my_uri, proteins_f, prot_matches_f, proteomes_f,
         tax_id = protein["taxon"]
         matches = prot_matches_s.get(acc, [])
         proteomes = proteomes_s.get(acc, [])
-        _structures = structures.get(acc, [])
+        structures = prot2struct.get(acc, [])
 
         l_taxa.append((tax_id, "protein", acc))
 
@@ -126,7 +138,7 @@ def count_xrefs(ora_uri, my_uri, proteins_f, prot_matches_f, proteomes_f,
             for entry_ac in _entries
         ]
 
-        for pdbe_id in _structures:
+        for pdbe_id in structures:
             l_structures.append((pdbe_id, "protein", acc))
 
             for entry_ac, entry_db in _entries:
