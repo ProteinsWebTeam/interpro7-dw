5af27acb7d42f4aacc623606590caabd3cb0c2fd
Matthias Blum <mat.blum@gmail.com>
2018-10-22 16:29:52 +0100

    PEP-8

--- i7dw/mysql.py
+++ i7dw/mysql.py
@@ -279,7 +279,10 @@ def insert_proteomes(ora_uri, my_uri, chunk_size=100000):
     data = []
     for p in proteomes:
         if p['tax_id'] not in taxa:
-            # if tax_id not in taxa, it's very likely that INTERPRO.ETAXI (source for taxonomy table) is out-of-date
+            """
+            If tax_id not in taxa, it's very likely that INTERPRO.ETAXI 
+            (source for taxonomy table) is out-of-date
+            """
             logging.warning('missing taxon (ID: {})'.format(p['tax_id']))
             continue
 
