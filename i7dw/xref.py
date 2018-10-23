b85348881b39f4c2ea4d71b55597fb663effa2d0
Matthias Blum <mat.blum@gmail.com>
2018-10-23 20:07:12 +0100

    Use latest version of Store

--- i7dw/xref.py
+++ i7dw/xref.py
@@ -16,119 +16,110 @@ logging.basicConfig(
 
 
 def feed_store(filepath: str, queue: Queue, **kwargs: dict):
-    with disk.KVStore(filepath, **kwargs) as store:
-        while True:
-            chunks = queue.get()
-            if chunks is None:
-                break
+    store = disk.Store(filepath, **kwargs)
 
-            for chunk in chunks:
-                store.add(*chunk)
+    while True:
+        chunk = queue.get()
+        if chunk is None:
+            break
 
-            store.flush()
+        for k, v in chunk:
+            store.update(k, v)
 
-        logging.info("{} filled".format(filepath))
+        store.flush()
 
-        tmp_usage = store.close()
-
-    logging.info("{} ready (temporary files: {} bytes)".format(
-        filepath, tmp_usage
+    logging.info("{} filled".format(filepath))
+    size = store.merge()
+    logging.info("{}: temporary files: {} bytes)".format(
+        filepath, size
     ))
 
 
-def count_xrefs(my_uri, proteins_f, prot_matches_f, proteomes_f,
-                entries_kvf, taxa_kvf, proteomes_kvf, sets_kvf,
-                structures_kvf, chunk_size=100000, compress=False,
-                tmpdir=None):
+def chunk_keys(keys: list, chunk_size: int=1000) -> list:
+    return [keys[i] for i in range(0, len(keys), chunk_size)]
 
-    try:
-        os.makedirs(tmpdir, exist_ok=True)
-    except (FileNotFoundError, TypeError):
-        tmpdir = None
+
+def count_xrefs(my_uri, src_proteins, src_matches, src_proteomes,
+                dst_entries, dst_taxa, dst_proteomes, dst_sets,
+                dst_structures, chunk_size=100000, tmpdir=None):
+
+    os.makedirs(tmpdir, exist_ok=True)
 
     entry2db = {
         acc: e["database"]
         for acc, e
         in mysql.get_entries(my_uri).items()
     }
-    entries = sorted(entry2db)
-    taxa = sorted(mysql.get_taxa(my_uri, slim=True))
-    proteomes = sorted(mysql.get_proteomes(my_uri))
-    sets = sorted(mysql.get_sets(my_uri, by_members=False))
+    entries = chunk_keys(sorted(entry2db))
+    taxa = chunk_keys(sorted(mysql.get_taxa(my_uri, method="basic")))
+    proteomes = chunk_keys(sorted(mysql.get_proteomes(my_uri)))
+    sets = chunk_keys(sorted(mysql.get_sets(my_uri)))
     structures = mysql.get_structures(my_uri)
 
-    prot2struct = {}
-    for s in structures.values():
-        for p in s["proteins"]:
-            if p in prot2struct:
-                prot2struct[p].add(s["accession"])
+    protein2pdb = {}
+    for pdb_id, s in structures.items():
+        for acc in s["proteins"]:
+            if acc in protein2pdb:
+                protein2pdb[acc].add(pdb_id)
             else:
-                prot2struct[p] = {s["accession"]}
+                protein2pdb[acc] = {pdb_id}
 
-    structures = sorted(structures)
+    structures = chunk_keys(sorted(structures))
 
-    l_entries = []
-    q_entries = Queue(maxsize=1)
-    p_entries = Process(target=feed_store,
-                        args=(entries_kvf, q_entries),
-                        kwargs={"compress": compress,
-                                "ids": entries,
-                                "tmpdir": tmpdir})
-
-    l_taxa = []
-    q_taxa = Queue(maxsize=1)
-    p_taxa = Process(target=feed_store,
-                     args=(taxa_kvf, q_taxa),
-                     kwargs={"compress": compress,
-                             "ids": taxa,
-                             "tmpdir": tmpdir})
-
-    l_proteomes = []
-    q_proteomes = Queue(maxsize=1)
-    p_proteomes = Process(target=feed_store,
-                          args=(proteomes_kvf, q_proteomes),
-                          kwargs={"compress": compress,
-                                  "ids": proteomes,
-                                  "tmpdir": tmpdir})
-
-    l_sets = []
-    q_sets = Queue(maxsize=1)
-    p_sets = Process(target=feed_store,
-                     args=(sets_kvf, q_sets),
-                     kwargs={"compress": compress,
-                             "ids": sets,
-                             "tmpdir": tmpdir})
-
-    l_structures = []
-    q_structures = Queue(maxsize=1)
-    p_structures = Process(target=feed_store,
-                           args=(structures_kvf, q_structures),
-                           kwargs={"compress": compress,
-                                   "ids": structures,
+    entries_chunk = []
+    entries_queue = Queue(maxsize=1)
+    entries_proc = Process(target=feed_store,
+                           args=(dst_entries, entries_queue),
+                           kwargs={"keys": entries,
                                    "tmpdir": tmpdir})
 
-    for p in (p_entries, p_taxa, p_proteomes, p_sets, p_structures):
+    taxa_chunk = []
+    taxa_queue = Queue(maxsize=1)
+    taxa_proc = Process(target=feed_store,
+                        args=(dst_taxa, taxa_queue),
+                        kwargs={"keys": taxa,
+                                "tmpdir": tmpdir})
+
+    proteomes_chunk = []
+    proteomes_queue = Queue(maxsize=1)
+    proteomes_proc = Process(target=feed_store,
+                             args=(dst_proteomes, proteomes_queue),
+                             kwargs={"keys": proteomes,
+                                     "tmpdir": tmpdir})
+
+    sets_chunk = []
+    sets_queue = Queue(maxsize=1)
+    sets_proc = Process(target=feed_store,
+                        args=(dst_sets, sets_queue),
+                        kwargs={"keys": sets,
+                                "tmpdir": tmpdir})
+
+    structures_chunk = []
+    structures_queue = Queue(maxsize=1)
+    structures_proc = Process(target=feed_store,
+                              args=(dst_structures, structures_queue),
+                              kwargs={"keys": structures,
+                                      "tmpdir": tmpdir})
+
+    for p in (entries_proc, taxa_proc, proteomes_proc, sets_proc,
+              structures_proc):
         p.start()
 
-    proteins_s = disk.Store(proteins_f)
-    prot_matches_s = disk.Store(prot_matches_f)
-    proteomes_s = disk.Store(proteomes_f)
+    proteins = disk.Store(src_proteins)
+    protein2matches = disk.Store(src_matches)
+    protein2proteome = disk.Store(src_proteomes)
 
     n_proteins = 0
     ts = time.time()
-    for acc, protein in proteins_s.iter():
+    for acc, protein in proteins:
         tax_id = protein["taxon"]
-        matches = prot_matches_s.get(acc, [])
-        proteomes = proteomes_s.get(acc, [])
-        structures = prot2struct.get(acc, [])
+        taxa_chunk.append((tax_id, {"protein": {acc}}))
 
-        l_taxa.append((tax_id, "protein", acc))
-
-        _entries = set()
-        for m in matches:
-            _entries.add(m["method_ac"])
-            if m["entry_ac"]:
-                _entries.add(m["entry_ac"])
+        _entries = {
+            m["entry_ac"]
+            for m in protein2matches.get(acc, [])
+            if m["entry_ac"]
+        }
 
         # Add source databases
         # TODO: remove `if in` check after debug
@@ -137,56 +128,77 @@ def count_xrefs(my_uri, proteins_f, prot_matches_f, proteomes_f,
             for entry_ac in _entries if entry_ac in entry2db
         ]
 
-        for pdbe_id in structures:
-            l_structures.append((pdbe_id, "protein", acc))
+        for pdbe_id in protein2pdb.get(acc, []):
+            structures_chunk.append((pdbe_id, {"protein": {acc}}))
 
             for entry_ac, entry_db in _entries:
-                l_structures.append((pdbe_id, "entry", entry_db, entry_ac))
-                l_entries.append((entry_ac, "structure", pdbe_id))
+                structures_chunk.append((pdbe_id,
+                                         {"entry": {entry_db: entry_ac}}))
+                entries_chunk.append((entry_ac,
+                                      {"structure": pdbe_id}))
 
         for upid in proteomes:
-            l_proteomes.append((upid, "protein", acc))
+            proteomes_chunk.append((upid, {"protein": {acc}}))
 
             for entry_ac, entry_db in _entries:
-                l_proteomes.append((upid, "entry", entry_db, entry_ac))
-                l_entries.append((entry_ac, "proteome", upid))
+                proteomes_chunk.append((upid,
+                                        {"entry": {entry_db: entry_ac}}))
+                entries_chunk.append((entry_ac, {"proteome": upid}))
 
         for entry_ac, entry_db in _entries:
-            l_entries.append((entry_ac, "protein", acc))
-            l_entries.append((entry_ac, "taxon", tax_id))
-            l_taxa.append((tax_id, "entry", entry_db, entry_ac))
+            entries_chunk.append((entry_ac, {"protein": {acc}}))
+            entries_chunk.append((entry_ac, {"taxon", tax_id}))
+            taxa_chunk.append((tax_id, {"entry": {entry_db: entry_ac}}))
 
         n_proteins += 1
 
         if not n_proteins % chunk_size:
-            q_entries.put(l_entries)
-            l_entries = []
-            q_taxa.put(l_taxa)
-            l_taxa = []
-            q_proteomes.put(l_proteomes)
-            l_proteomes = []
-            q_sets.put(l_sets)
-            l_sets = []
-            q_structures.put(l_structures)
-            l_structures = []
+            entries_queue.put(entries_chunk)
+            entries_chunk = []
+            taxa_queue.put(taxa_chunk)
+            taxa_chunk = []
+            proteomes_queue.put(proteomes_chunk)
+            proteomes_chunk = []
+            sets_queue.put(sets_chunk)
+            sets_chunk = []
+            structures_queue.put(structures_chunk)
+            structures_chunk = []
 
         if not n_proteins % 1000000:
             logging.info('{:>12} ({:.0f} proteins/sec)'.format(
-                n_proteins, n_proteins // (time.time() - ts)
+                n_proteins, n_proteins / (time.time() - ts)
             ))
 
+    if entries_chunk:
+        entries_queue.put(entries_chunk)
+        entries_chunk = []
+    if taxa_chunk:
+        taxa_queue.put(taxa_chunk)
+        taxa_chunk = []
+    if proteomes_chunk:
+        proteomes_queue.put(proteomes_chunk)
+        proteomes_chunk = []
+    if sets_chunk:
+        sets_queue.put(sets_chunk)
+        sets_chunk = []
+    if structures_chunk:
+        structures_queue.put(structures_chunk)
+        structures_chunk = []
+
     logging.info('{:>12} ({:.0f} proteins/sec)'.format(
-        n_proteins, n_proteins // (time.time() - ts)
+        n_proteins, n_proteins / (time.time() - ts)
     ))
 
-    proteins_s.close()
-    prot_matches_s.close()
-    proteomes_s.close()
+    proteins.close()
+    protein2matches.close()
+    protein2proteome.close()
 
-    for q in (q_entries, q_taxa, q_proteomes,  q_sets, q_structures):
+    for q in (entries_queue, taxa_queue, proteomes_queue, sets_queue,
+              structures_queue):
         q.put(None)
         
-    for p in (p_entries, p_taxa, p_proteomes,  p_sets, p_structures):
+    for p in (entries_proc, taxa_proc, proteomes_proc, sets_proc,
+              structures_proc):
         p.join()
 
     logging.info("complete")
