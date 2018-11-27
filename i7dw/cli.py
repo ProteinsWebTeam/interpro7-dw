a9777a4d236e7cc38ae27a6e909ce3f1d4d4d56d
Matthias Blum <mat.blum@gmail.com>
2018-11-27 02:07:17 +0000

    adjust processes param

--- i7dw/cli.py
+++ i7dw/cli.py
@@ -345,7 +345,7 @@ def cli():
                 os.path.join(export_dir, "structures_xref.dat"),
                 os.path.join(export_dir, "taxa_xref.dat")
             ),
-            kwargs=dict(processes=4),
+            kwargs=dict(processes=3),
             scheduler=dict(queue=queue, mem=16000, tmp=15000, cpu=4),
             requires=[
                 "export-xrefs", "insert-proteins", "overlapping-families"
