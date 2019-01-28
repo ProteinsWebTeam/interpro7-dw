de4c61da3dc1eafaca5a977806af7a13e238a1a5
Matthias Blum <mat.blum@gmail.com>
2019-01-28 11:46:22 +0000

    Removing dead code

--- i7dw/dbms.py
+++ i7dw/dbms.py
@@ -9,8 +9,6 @@ import MySQLdb.cursors
 
 
 def connect(uri, sscursor=False, encoding='utf-8'):
-    # driver:user/password@host:port/dbname[:encoding]
-    # m = re.match(r'(\w+):([^/]+)/([^@]+)@([^:]+):(\d+)/(\w+)(?::([\w-]+))?', uri)
     m = re.match(r'(\w+):([^/]+)/([^@]+)@([^:]+):(\d+)/(\w+)', uri)
 
     if m is None:
@@ -25,7 +23,8 @@ def connect(uri, sscursor=False, encoding='utf-8'):
 
     if driver == 'oracle':
         dsn = cx_Oracle.makedsn(host, port, db)
-        con = cx_Oracle.connect(user, passwd, dsn, encoding=encoding, nencoding=encoding)
+        con = cx_Oracle.connect(user, passwd, dsn,
+                                encoding=encoding, nencoding=encoding)
         return con, con.cursor()
     elif driver == 'mysql':
         # supports 'utf8', not 'utf-8'
