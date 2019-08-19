3923379e7d5b1991f9cb82381f406bae71b55919
Matthias Blum <mat.blum@gmail.com>
2019-08-19 23:42:06 +0100

    update resources requirements

--- i7dw/cli.py
+++ i7dw/cli.py
@@ -397,7 +397,7 @@ def build_dw():
                 os.path.join(export_dir, "entries.dat")
             ),
             kwargs=dict(processes=4, tmpdir="/scratch"),
-            scheduler=dict(queue=queue, mem=16000, scratch=40000, cpu=4),
+            scheduler=dict(queue=queue, mem=16000, scratch=15000, cpu=4),
             requires=["export-proteins", "export-matches", "export-proteomes",
                       "export-ida", "insert-structures", "insert-sets"]
         ),
