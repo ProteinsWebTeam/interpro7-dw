ce04e208b239314d730c762a2a49ac3829a9804a
Matthias Blum <mat.blum@gmail.com>
2019-09-27 18:43:18 +0100

    Adding key function to sort locations

--- i7dw/interpro/__init__.py
+++ i7dw/interpro/__init__.py
@@ -7,64 +7,22 @@ import cx_Oracle
 import MySQLdb
 
 from i7dw import io, logger
-# from .mysql.entry import get_entries  # TODO fix import
 
 
 MIN_OVERLAP = 0.1
 
 
-def extract_frag(frag: dict) -> tuple:
+def extract_loc(loc: Dict) -> Tuple[int, int]:
+    # Assume fragments are sorted: use leftmost fragment
+    return extract_frag(loc["fragments"][0])
+
+
+def extract_frag(frag: dict) -> Tuple[int, int]:
     return frag["start"], frag["end"]
 
 
 def is_overlapping(x1: int, x2: int, y1: int, y2: int) -> bool:
     return x1 <= y2 and y1 <= x2
-
-
-def condense(to_condense: Dict[str, List]) -> Dict[str, List]:
-    condensed = {}
-    for entry_ac, locations in to_condense.items():
-        start = end = None
-        _locations = []
-
-        # We assume `locations` and `fragments` are sorted
-        for fragments in locations:
-            # We do not consider fragmented matches:
-            s = fragments[0]["start"]
-            e = fragments[-1]["end"]
-
-            if start is None:
-                start = s
-                end = e
-                continue
-            elif s <= end:
-                # matches are overlapping (at least one residue)
-                overlap = min(end, e) - max(start, s) + 1
-                shortest = min(end - start, e - s) + 1
-
-                if overlap >= shortest * MIN_OVERLAP:
-                    # Merge
-                    end = e
-                    continue
-
-            _locations.append((start, end))
-            start = s
-            end = e
-
-        _locations.append((start, end))
-
-        condensed[entry_ac] = []
-        for start, end in _locations:
-            condensed[entry_ac].append({
-                "fragments": [{
-                    "start": start,
-                    "end": end,
-                    "dc-status": "CONTINUOUS"
-                }],
-                "model_acc": None
-            })
-
-    return condensed
 
 
 def export_ida(url: str, src_matches: str, dst_ida: str,
@@ -153,7 +111,6 @@ class Populator(object):
             return
 
         self.cur.executemany(self.query, self.rows)
-        self.count += len(self.rows)
         self.rows = []
 
         if self.autocommit:
