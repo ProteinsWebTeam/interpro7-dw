f389276b2a9bdbc65e9839f483d6ab9ee3500973
Matthias Blum <mat.blum@gmail.com>
2019-10-03 13:26:20 +0100

    Refactoring insert_proteins()

--- i7dw/interpro/mysql/proteins.py
+++ i7dw/interpro/mysql/proteins.py
@@ -1,41 +1,18 @@
 # -*- coding: utf-8 -*-
 
-import hashlib
 import json
-from typing import Dict, List, Optional, Tuple
+from typing import Optional
 
 import MySQLdb
 
 from i7dw import io, logger, pdbe
-from i7dw.interpro import Populator, oracle, extract_frag, MIN_OVERLAP
+from i7dw.interpro import MIN_OVERLAP
+from i7dw.interpro import DomainArchitecture, Populator, extract_frag
+from i7dw.interpro import oracle
 from .entries import get_entries, iter_sets
 from .structures import iter_structures
 from .taxonomy import get_taxa
-from .utils import parse_url
-
-
-def calculate_domain_architecture(pfam_entries: List[Tuple]) -> Tuple[str, str]:
-    """
-    Tuple:
-      Pfam acc: str, InterPro acc: Optional[str], locations: List[Dict]
-
-    Sort Pfam entries based on the leftmost fragment of the leftmost location
-        - e[2]          -> access locations
-        - e[2][0]       -> access leftmost location
-        - e[2][0][0]    -> access left fragment
-    """
-    pfam_entries.sort(key=lambda e: extract_frag(e[2][0][0]))
-
-    dom_arch = []
-    for pfam_acc, interpro_acc, locations in pfam_entries:
-        if interpro_acc:
-            dom_arch.append(pfam_acc + ':' + interpro_acc)
-        else:
-            dom_arch.append(pfam_acc)
-
-    dom_arch = '-'.join(dom_arch)
-    dom_arch_id = hashlib.sha1(dom_arch.encode("utf-8")).hexdigest()
-    return dom_arch, dom_arch_id
+from .utils import drop_index, parse_url
 
 
 def insert_proteins(my_url: str, ora_ippro_url: str, ora_pdbe_url: str,
@@ -50,20 +27,20 @@ def insert_proteins(my_url: str, ora_ippro_url: str, ora_pdbe_url: str,
     scop_domains = pdbe.get_scop_domains(ora_pdbe_url)
 
     # Structural predictions (ModBase and Swiss-Model models)
-    protein2predictions = oracle.get_structural_predictions(ora_ippro_url)
+    sec_predictions = oracle.get_structural_predictions(ora_ippro_url)
 
     # MySQL data
     taxa = get_taxa(my_url, lineage=False)
     entries = get_entries(my_url)
 
-    protein2structures = {}
+    structures = {}
     for s in iter_structures(my_url):
         pdbe_id = s["accession"]
         for protein_acc in s["proteins"]:
             try:
-                protein2structures[protein_acc].add(pdbe_id)
+                structures[protein_acc].add(pdbe_id)
             except KeyError:
-                protein2structures[protein_acc] = {pdbe_id}
+                structures[protein_acc] = {pdbe_id}
 
     entry2set = {}
     for s in iter_sets(my_url):
@@ -71,27 +48,37 @@ def insert_proteins(my_url: str, ora_ippro_url: str, ora_pdbe_url: str,
         for entry_acc in s["members"]:
             entry2set[entry_acc] = set_acc
 
+    logger.info("calculating domain architectures")
     proteins = io.Store(src_proteins)
-    protein2sequence = io.Store(src_sequences)
-    protein2misc = io.Store(src_misc)
-    protein2names = io.Store(src_names)
-    protein2comments = io.Store(src_comments)
-    protein2proteome = io.Store(src_proteomes)
-    protein2residues = io.Store(src_residues)
-    protein2features = io.Store(src_features)
-    protein2matches = io.Store(src_matches)
+    matches = io.Store(src_matches)
+    domains = io.Store(keys=matches.keys, tmpdir=tmpdir)
+    dom_arch = DomainArchitecture(entries)
+    dom_cnts = {}
+    n_proteins = 0
+    for protein_acc, protein_info in proteins:
+        dom_arch.update(matches.get(protein_acc, {}))
+        dom_id = dom_arch.identifier
+        domains[protein_acc] = (dom_id, dom_arch.hash)
 
-    logger.info("truncating table")
+        try:
+            dom_cnts[dom_id] += 1
+        except KeyError:
+            dom_cnts[dom_id] = 1
+
+        n_proteins += 1
+        if not n_proteins % 1000000:
+            domains.sync()
+
+    size = domains.merge()
+    logger.info(f"\ttemporary files: {size/1024/1024:.0f} MB")
+
+    logger.info("preparing MySQL")
     con = MySQLdb.connect(**parse_url(my_url), charset="utf8")
     cur = con.cursor()
     cur.execute("TRUNCATE TABLE webfront_protein")
-    for index in ("ui_webfront_protein_identifier",
-                  "i_webfront_protein_length",
-                  "i_webfront_protein_ida"):
-        try:
-            cur.execute(f"DROP INDEX {index} ON webfront_protein")
-        except Exception:
-            pass
+    for idx in ("ui_webfront_protein_identifier", "i_webfront_protein_length",
+                "i_webfront_protein_ida", "i_webfront_protein_fragment"):
+        drop_index(cur, "webfront_protein", idx)
 
     logger.info("counting isoforms")
     cur.execute(
@@ -118,52 +105,52 @@ def insert_proteins(my_url: str, ora_ippro_url: str, ora_pdbe_url: str,
     """
 
     with Populator(con, query) as table:
+        sequences = io.Store(src_sequences)
+        misc = io.Store(src_misc)
+        names = io.Store(src_names)
+        comments = io.Store(src_comments)
+        proteomes = io.Store(src_proteomes)
+        residues = io.Store(src_residues)
+        features = io.Store(src_features)
         n_proteins = 0
 
-        for protein_acc, protein in proteins:
-            tax_id = protein["taxon"]
+        for protein_acc, protein_info in proteins:
+            tax_id = protein_info["taxon"]
             try:
                 taxon = taxa[tax_id]
             except KeyError:
                 continue
 
             try:
-                sequence = protein2sequence[protein_acc]
+                sequence = sequences[protein_acc]
             except KeyError:
                 continue
 
             try:
-                evidence, gene = protein2misc[protein_acc]
+                evidence, gene = misc[protein_acc]
             except KeyError:
                 continue
 
             if not evidence:
                 continue
 
-            if protein["length"] <= 100:
+            if protein_info["length"] <= 100:
                 size = "small"
-            elif protein["length"] <= 1000:
+            elif protein_info["length"] <= 1000:
                 size = "medium"
             else:
                 size = "large"
 
-            name, other_names = protein2names.get(protein_acc, (None, None))
-            upid = protein2proteome.get(protein_acc)
-
             go_terms = {}  # InterPro2GO + InterPro matches -> UniProt-GO
-            protein2entries = {}
-            protein2sets = set()
-            pfam_entries = []
-            for entry_acc, locations in protein2matches.get(protein_acc, {}).items():
+            protein_entries = {}
+            protein_sets = set()
+            for entry_acc, locations in matches.get(protein_acc, {}).items():
                 e = entries[entry_acc]
                 database = e["database"]
                 try:
-                    protein2entries[database] += 1
+                    protein_entries[database] += 1
                 except KeyError:
-                    protein2entries[database] = 1
-
-                if database == "pfam":
-                    pfam_entries.append((entry_acc, e["integrated"], locations))
+                    protein_entries[database] = 1
 
                 for term in e["go_terms"]:
                     go_terms[term["identifier"]] = term
@@ -173,65 +160,57 @@ def insert_proteins(my_url: str, ora_ippro_url: str, ora_pdbe_url: str,
                 except KeyError:
                     pass
                 else:
-                    protein2sets.add(set_acc)
+                    protein_sets.add(set_acc)
 
             protein_entries["total"] = sum(protein_entries.values())
-
-            structures = {
-                "feature": {
-                    "cath": {},
-                    "scop": {}
-                },
-                "prediction": protein2predictions.get(protein_acc, {})
+            protein_structures = {
+                "feature": {"cath": {}, "scop": {}},
+                "prediction": sec_predictions.get(protein_acc, {})
             }
-            cath_features = {}
-            scop_features = {}
-            for pdbe_id in protein2structures.get(protein_acc, []):
+            for pdbe_id in structures.get(protein_acc, []):
                 for dom_id, dom in cath_domains.get(pdbe_id, {}).items():
-                    structures["feature"]["cath"][dom_id] = dom
+                    protein_structures["feature"]["cath"][dom_id] = dom
 
                 for dom_id, dom in scop_domains.get(pdbe_id, {}).items():
-                    structures["feature"]["scop"][dom_id] = dom
+                    protein_structures["feature"]["scop"][dom_id] = dom
 
-            dom_arch = protein2ida.get(acc)
-            if dom_arch:
-                ida, ida_id = dom_arch
-                num_ida = ida_counts[ida_id]
-            else:
-                ida = ida_id = None
-                num_ida = 0
+            name, other_names = names.get(protein_acc, (None, None))
+            upid = proteomes.get(protein_acc)
+
+            dom_id, dom_hash = domains[protein_acc]
+            dom_cnt = dom_cnts[dom_id]
 
             # Enqueue record for protein table
             table.insert((
                 protein_acc,
-                protein["identifier"],
+                protein_info["identifier"],
                 json.dumps(taxon),
                 name,
                 json.dumps(other_names),
-                json.dumps(protein2comments.get(acc, [])),
+                json.dumps(comments.get(protein_acc, [])),
                 sequence,
-                protein["length"],
+                protein_info["length"],
                 size,
                 upid,
                 gene,
                 json.dumps(list(go_terms.values())),
                 evidence,
-                "reviewed" if protein["is_reviewed"] else "unreviewed",
-                json.dumps(protein2residues.get(acc, {})),
-                1 if protein["is_fragment"] else 0,
-                json.dumps(structures),
+                "reviewed" if protein_info["is_reviewed"] else "unreviewed",
+                json.dumps(residues.get(protein_acc, {})),
+                1 if protein_info["is_fragment"] else 0,
+                json.dumps(protein_structures),
                 tax_id,
-                json.dumps(protein2features.get(acc, {})),
-                ida_id,
-                ida,
+                json.dumps(features.get(protein_acc, {})),
+                dom_id,
+                dom_hash,
                 json.dumps({
-                    "entries": protein2entries,
-                    "structures": len(protein2pdb.get(acc, [])),
-                    "sets": len(protein2sets),
+                    "entries": protein_entries,
+                    "structures": len(structures.get(protein_acc, [])),
+                    "sets": len(protein_sets),
                     "proteomes": 1 if upid else 0,
                     "taxa": 1,
-                    "idas": num_ida,
-                    "isoforms": isoforms.get(acc, 0)
+                    "idas": dom_cnt,
+                    "isoforms": isoforms.get(protein_acc, 0)
                 })
             ))
 
