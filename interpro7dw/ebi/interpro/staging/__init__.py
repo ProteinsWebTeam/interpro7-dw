0776b1f59a81c880c9ddc65000041dfdd1649eaa
Matthias Blum <mat.blum@gmail.com>
2020-02-24 17:24:43 +0000

    Code to generate release notes

--- interpro7dw/ebi/interpro/staging/__init__.py
+++ interpro7dw/ebi/interpro/staging/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from .database import insert_databases
+from .database import insert_databases, make_release_notes
 from .entry import init_clans, insert_annotations, export_overlapping_entries
 from .protein import export_ida, insert_isoforms, insert_proteins
--- interpro7dw/ebi/interpro/staging/database.py
+++ interpro7dw/ebi/interpro/staging/database.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
+import json
+
 import MySQLdb
 
+from interpro7dw import logger
 from interpro7dw.ebi.interpro import production as ippro
 from interpro7dw.ebi.interpro.utils import Table
-from interpro7dw.utils import url2dict
+from interpro7dw.utils import Store, dataload, url2dict
 
 
 def insert_databases(pro_url: str, stg_url: str):
@@ -37,3 +40,292 @@ def insert_databases(pro_url: str, stg_url: str):
 
     con.commit()
     con.close()
+
+
+def make_release_notes(src_entries: str, src_proteins: str, src_proteomes: str,
+                       src_structures: str, src_taxonomy: str,
+                       src_uniprot2matches: str, src_uniprot2proteome: str,
+                       rel_url: str, stg_url: str):
+    logger.info("preparing data")
+    signatures = set()
+    for entry in dataload(src_entries).values():
+        if entry.integrated_in:
+            signatures.add(entry.accession)
+
+    uniprot2pdbe = {}
+    for pdb_id, entry in dataload(src_structures).items():
+        for uniprot_acc in entry["proteins"]:
+            try:
+                uniprot2pdbe[uniprot_acc].add(pdb_id)
+            except KeyError:
+                uniprot2pdbe[uniprot_acc] = {pdb_id}
+
+    con = MySQLdb.connect(**url2dict(stg_url))
+    cur = con.cursor()
+    cur.execute(
+        """
+        SELECT name_long, version
+        FROM webfront_database
+        WHERE name_long IN ('UniProtKB', 'UniProtKB/Swiss-Prot', 'UniProtKB/TrEMBL')
+        """
+    )
+    uniprot = {}
+    for name, version in cur:
+        uniprot[name] = {
+            "version": version,
+            "count": 0,
+            "signatures": 0,
+            "integrated_signatures": 0
+        }
+    cur.close()
+    con.close()
+
+    proteins = Store(src_proteins)
+    uniprot2matches = Store(src_uniprot2matches)
+    uniprot2proteome = Store(src_uniprot2proteome)
+
+    # Entities found in InterPro
+    integrated_proteomes = set()
+    integrated_structures = set()
+    integrated_taxonomy = set()
+
+    i = 0
+    for protein_acc, info in proteins.items():
+        if info["reviewed"]:
+            database = uniprot["UniProtKB/Swiss-Prot"]
+        else:
+            database = uniprot["UniProtKB/TrEMBL"]
+
+        database["count"] += 1
+
+        try:
+            entries = uniprot2matches[protein_acc]
+        except KeyError:
+            pass
+        else:
+            # Protein has a least one signature
+            database["signatures"] += 1
+
+            for entry_acc in entries:
+                if entry_acc in signatures:
+                    # At least one integrated signature
+                    database["integrated_signatures"] += 1
+
+                    try:
+                        proteome_id = uniprot2proteome[protein_acc]
+                    except KeyError:
+                        pass
+                    else:
+                        integrated_proteomes.add(proteome_id)
+
+                    try:
+                        pdb_ids = uniprot2pdbe[protein_acc]
+                    except KeyError:
+                        pass
+                    else:
+                        integrated_structures |= pdb_ids
+
+                    integrated_taxonomy.add(info["taxid"])
+                    break
+
+        i += 1
+        if not i % 10000000:
+            logger.info(f"{i:>12,}")
+
+    proteins.close()
+    uniprot2matches.close()
+    uniprot2proteome.close()
+
+    logger.info(f"{i:>12,}")
+
+    # Sum Swiss-Prot and TrEMBL counts
+    for key in ["count", "signatures", "integrated_signatures"]:
+        value_sp = uniprot["UniProtKB/Swiss-Prot"][key]
+        value_tr = uniprot["UniProtKB/TrEMBL"][key]
+        uniprot["UniProtKB"][key] = value_sp + value_tr
+
+    logger.info("tracking changes since last releases")
+    con = MySQLdb.connect(**url2dict(rel_url))
+    cur = con.cursor()
+    cur.execute(
+        """
+        SELECT accession, source_database, integrated_id
+        FROM webfront_entry 
+        WHERE is_alive = 1
+        """
+    )
+    public_entries = set()
+    public_integrated = set()
+    for entry_acc, database, integrated_in in cur:
+        if database == "interpro":
+            public_entries.add(entry_acc)
+        elif integrated_in:
+            # Signature already integrated in the previous release
+            public_integrated.add(entry_acc)
+
+    cur.execute(
+        """
+        SELECT name, version 
+        FROM webfront_database 
+        WHERE type = 'entry'
+        """
+    )
+    public_databases = dict(cur.fetchall())
+    cur.close()
+    con.close()
+
+    con = MySQLdb.connect(**url2dict(stg_url))
+    cur = con.cursor()
+    cur.execute(
+        """
+        CREATE TABLE IF NOT EXISTS webfront_release_note
+        (
+            version VARCHAR(20) PRIMARY KEY NOT NULL,
+            release_date DATETIME NOT NULL,
+            content LONGTEXT NOT NULL
+        ) CHARSET=utf8 DEFAULT COLLATE=utf8_unicode_ci
+        """
+    )
+    cur.execute(
+        """
+        SELECT name, name_long, version, release_date
+        FROM webfront_database 
+        WHERE type = 'entry'
+        """
+    )
+    staging_databases = {row[0]: (row[1], row[2], row[3]) for row in cur}
+
+    interpro_new = []
+    interpro_types = {}
+    member_databases = {}
+    pubmed_citations = set()
+    interpro2go = None
+    latest_entry = None
+
+    for entry in sorted(dataload(src_entries).values(), key=lambda e: e.creation_date):
+        if not entry.is_alive:
+            continue
+
+        if entry.database == "interpro":
+            for pub in entry.literature.values():
+                if pub["PMID"] is not None:
+                    pubmed_citations.add(pub["PMID"])
+
+            try:
+                interpro_types[entry.type] += 1
+            except KeyError:
+                interpro_types[entry.type] = 1
+
+            if entry.accession not in public_entries:
+                interpro_new.append(entry.accession)
+
+            interpro2go += len(entry.go_terms)
+            latest_entry = entry.accession
+        else:
+            try:
+                obj = member_databases[entry.database]
+            except KeyError:
+                database, version, _ = staging_databases[entry.database]
+
+                is_new = is_updated = False
+                if entry.database not in public_databases:
+                    is_new = True
+                elif version != public_databases[entry.database]:
+                    is_updated = True
+
+                obj = member_databases[entry.database] = {
+                    "name": database,
+                    "version": version,
+                    "signatures": 0,
+                    "integrated_signatures": 0,
+                    "recently_integrated": [],
+                    "is_new": is_new,
+                    "is_updated": is_updated,
+                    "sets": set()
+                }
+
+            obj["signatures"] += 1
+            if entry.integrated_in:
+                obj["integrated_signatures"] += 1
+
+                if entry.accession not in public_integrated:
+                    # Recent integration
+                    obj["recently_integrated"].append(entry.accession)
+
+            if entry.clan:
+                obj["sets"].add(entry.clan)
+
+    # Transform sets of clans to counts:
+    for obj in member_databases.values():
+        obj["sets"] = len(obj["sets"])
+
+    structures = list(dataload(src_structures).values())
+
+    proteomes = set(dataload(src_proteomes).keys())
+    errors = integrated_proteomes - proteomes
+    if errors:
+        raise RuntimeError(f"{len(errors)} invalid proteomes")
+
+    taxa = set(dataload(src_taxonomy).keys())
+    errors = integrated_taxonomy - taxa
+    if errors:
+        raise RuntimeError(f"{len(errors)} invalid taxa")
+
+    content = {
+        "interpro": {
+            "entries": sum(interpro_types.values()),
+            "new_entries": interpro_new,
+            "latest_entry": latest_entry,
+            "types": interpro_types,
+            "go_terms": interpro2go
+        },
+        "member_databases": member_databases,
+        "proteins": uniprot,
+        "structures": {
+            "total": len(structures),
+            "integrated": len(integrated_structures),
+            "version": max(entry["data"] for entry in structures)
+        },
+        "proteomes": {
+            "total": len(proteomes),
+            "integrated": len(integrated_proteomes),
+            "version": uniprot["UniProtKB"]["version"]
+        },
+        "taxonomy": {
+            "total": len(taxa),
+            "integrated": len(integrated_taxonomy),
+            "version": uniprot["UniProtKB"]["version"]
+        },
+        "citations": len(pubmed_citations)
+    }
+
+    _, version, date = staging_databases["interpro"]
+    cur.execute(
+        """
+        SELECT COUNT(*)
+        FROM webfront_release_note
+        WHERE version = %s
+        """, (version,)
+    )
+    n_rows, = cur.fetchone()
+
+    if n_rows:
+        cur.execute(
+            """
+            UPDATE webfront_release_note
+            SET content = %s
+            WHERE version = %s
+            """, (json.dumps(content), version)
+        )
+    else:
+        cur.execute(
+            """
+            INSERT INTO webfront_release_note
+            VALUES (%s, %s, %s)
+            """, (version, date, json.dumps(content) )
+        )
+
+    con.commit()
+    cur.close()
+    con.close()
+    logger.info("complete")
