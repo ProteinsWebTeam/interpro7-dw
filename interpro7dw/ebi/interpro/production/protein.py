a12f42870482489e0ada2118b5fdcd5ea65aaf87
Matthias Blum <mat.blum@gmail.com>
2020-03-25 19:01:19 +0000

    Unnecessary JOIN

--- interpro7dw/ebi/interpro/production/protein.py
+++ interpro7dw/ebi/interpro/production/protein.py
@@ -387,10 +387,8 @@ def get_isoforms(url: str):
 
     cur.execute(
         """
-        SELECT V.PROTEIN_AC, V.VARIANT, V.LENGTH, P.SEQ_SHORT, P.SEQ_LONG
-        FROM INTERPRO.VARSPLIC_MASTER V
-        INNER JOIN UNIPARC.PROTEIN P 
-          ON V.CRC64 = P.CRC64
+        SELECT PROTEIN_AC, VARIANT, LENGTH, SEQ_SHORT, SEQ_LONG
+        FROM INTERPRO.VARSPLIC_MASTER
         """
     )
 
