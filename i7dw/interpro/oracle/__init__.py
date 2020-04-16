c75e65f105671d844270783276eeaf025ba24ab7
Matthias Blum <mat.blum@gmail.com>
2020-04-16 11:36:47 +0100

    Removing old files

--- i7dw/interpro/oracle/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import export, utils
-from ._data import (get_databases, get_entries, get_deleted_entries,
-                    get_name_history, get_integration_history, get_clans,
-                    get_clan_alignments, get_isoforms, get_taxa)
