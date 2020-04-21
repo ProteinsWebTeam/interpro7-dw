af8cd580b9aca3d605552b3b6d761dcbe7b20e2c
Matthias Blum <mat.blum@gmail.com>
2020-04-21 13:40:27 +0100

    Adding DataDumpCollection

--- interpro7dw/utils.py
+++ interpro7dw/utils.py
@@ -611,3 +611,78 @@ def merge_dumps(files: Sequence[str], replace: bool=False):
     finally:
         for datadump in iterables:
             datadump.close()
+
+
+class DataDumpCollection(object):
+    def __init__(self, files: Sequence[str]):
+        self.files = files
+
+    def __iter__(self):
+        return self.keys()
+
+    def keys(self):
+        iterables = [DataDump(path) for path in self.files]
+        _key = None
+
+        try:
+            for key, xrefs in heapq.merge(*iterables, key=lambda x: x[0]):
+                if key != _key:
+                    if _key is not None:
+                        yield _key
+
+                    _key = key
+
+            if _key is not None:
+                yield _key
+        finally:
+            for datadump in iterables:
+                datadump.close()
+
+    def items(self, replace: bool=False):
+        iterables = [DataDump(path) for path in self.files]
+        _key = None
+        _xrefs = None
+
+        try:
+            for key, xrefs in heapq.merge(*iterables, key=lambda x: x[0]):
+                if key == _key:
+                    deepupdate(xrefs, _xrefs, replace=replace)
+                else:
+                    if _key is not None:
+                        yield _key, _xrefs
+
+                    _key = key
+                    _xrefs = xrefs
+
+            if _key is not None:
+                yield _key, _xrefs
+        finally:
+            for datadump in iterables:
+                datadump.close()
+
+    def range(self, start, stop: Optional=None, replace: bool=False):
+        iterables = [DataDump(path) for path in self.files]
+        _key = None
+        _xrefs = None
+
+        try:
+            for key, xrefs in heapq.merge(*iterables, key=lambda x: x[0]):
+                if key < start:
+                    continue
+                elif stop is None or key < stop:
+                    if key == _key:
+                        deepupdate(xrefs, _xrefs, replace=replace)
+                    else:
+                        if _key is not None:
+                            yield _key, _xrefs
+
+                        _key = key
+                        _xrefs = xrefs
+                else:
+                    break
+
+            if _key is not None:
+                yield _key, _xrefs
+        finally:
+            for datadump in iterables:
+                datadump.close()
