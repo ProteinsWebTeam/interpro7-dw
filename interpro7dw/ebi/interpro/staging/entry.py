dfa384fc6bc315d99109a16da86dfb88c46d6aef
Matthias Blum <mat.blum@gmail.com>
2020-05-12 11:11:07 +0100

    Adding index on Pfam accessions

--- interpro7dw/ebi/interpro/staging/entry.py
+++ interpro7dw/ebi/interpro/staging/entry.py
@@ -417,6 +417,8 @@ def insert_annotations(pfam_url: str, stg_url: str):
             table.insert((anno_id, acc, anno_type, value, mime))
 
     con.commit()
+    cur.execute("CREATE INDEX i_entryannotation "
+                "ON webfront_entryannotation (accession_id)")
     con.close()
 
 
