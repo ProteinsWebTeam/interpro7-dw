899623d98ff4cd99d9027b49812667ab365e3104
Matthias Blum <mat.blum@gmail.com>
2021-10-04 16:41:16 +0100

    Create module for exporting proteomes

---
+++ interpro7dw/uniprot/__init__.py
@@ -0,0 +1,2 @@
+from . import proteins, proteomes
+
--- interpro7dw/uniprot/proteins.py
+++ interpro7dw/uniprot/proteins.py
@@ -3,7 +3,7 @@ from typing import List, Sequence, Tuple
 import cx_Oracle
 
 from interpro7dw.utils import logger
-from interpro7dw.utils.store import Store, dumpobj
+from interpro7dw.utils.store import Store
 
 
 def export_entry2functions(url: str, src: str, dst: str, **kwargs):
@@ -218,39 +218,3 @@ def export_entry2proteome(url: str, src: str, dst: str, **kwargs):
         logger.info(f"temporary files: {store.size / 1024 / 1024:.0f} MB")
 
     logger.info("done")
-
-
-def export_proteomes(url: str, file: str):
-    con = cx_Oracle.connect(url)
-    cur = con.cursor()
-    cur.execute(
-        """
-        SELECT P.UPID, P.PROTEOME_NAME, P.IS_REFERENCE, P.GC_SET_ACC, 
-          TO_CHAR(P.PROTEOME_TAXID), SN.NAME
-        FROM SPTR.PROTEOME P
-        LEFT OUTER JOIN TAXONOMY.SPTR_STRAIN S
-          ON P.PROTEOME_TAXID = S.TAX_ID
-        LEFT OUTER JOIN TAXONOMY.SPTR_STRAIN_NAME SN
-          ON S.STRAIN_ID = SN.STRAIN_ID
-        WHERE P.IS_REFERENCE = 1
-        """
-    )
-
-    proteomes = {}
-    for upid, name, is_reference, assembly, taxon_id, strain in cur:
-        if upid in proteomes:
-            continue
-
-        proteomes[upid] = {
-            "id": upid,
-            "name": name,
-            "is_reference": is_reference != 0,
-            "assembly": assembly,
-            "taxon_id": taxon_id,
-            "strain": strain
-        }
-
-    cur.close()
-    con.close()
-
-    dumpobj(proteomes, file)
