0470b7933dd48ca3f6ed22cf1602a377180c54a7
Matthias Blum <mat.blum@gmail.com>
2021-10-15 15:54:32 +0100

    Function to insert entry annotations (Pfam alignments and HMMs)

--- interpro7dw/interpro/mysql/entries.py
+++ interpro7dw/interpro/mysql/entries.py
@@ -7,6 +7,97 @@ from interpro7dw.utils.store import loadobj, SimpleStore
 from .utils import jsonify
 
 
+def insert_annotations(url: str, hmms_file: str, pfam_alignments: str):
+    logger.info("creating webfront_entryannotation")
+    con = MySQLdb.connect(**url2dict(url))
+    cur = con.cursor()
+    cur.execute("DROP TABLE IF EXISTS webfront_entryannotation")
+    cur.execute(
+        """
+        CREATE TABLE webfront_entryannotation
+        (
+            annotation_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            accession VARCHAR(25) NOT NULL,
+            type VARCHAR(20) NOT NULL,
+            value LONGBLOB NOT NULL,
+            mime_type VARCHAR(32) NOT NULL,
+            num_sequences INT
+        ) CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci
+        """
+    )
+
+    for file in (hmms_file, pfam_alignments):
+        with SimpleStore(file) as store:
+            for accession, anno_type, anno_value, count in store:
+                if anno_type == "logo":
+                    mime_type = "application/json"
+                else:
+                    mime_type = "application/gzip"
+
+                cur.execute(
+                    """
+                    INSERT INTO webfront_entryannotation (
+                        accession, type, value, mime_type, num_sequences
+                    ) VALUES (%s, %s, %s, %s, %s)
+                    """, (accession, anno_type, anno_value, mime_type, count)
+                )
+
+    con.commit()
+
+    logger.info("indexing")
+    cur.execute(
+        """
+        CREATE INDEX i_entryannotation 
+        ON webfront_entryannotation (accession)
+        """
+    )
+
+    cur.close()
+    con.close()
+
+    logger.info("done")
+
+
+def insert_databases(url: str, databases_file: str):
+    logger.info("creating webfront_database")
+    con = MySQLdb.connect(**url2dict(url), charset="utf8mb4")
+    cur = con.cursor()
+    cur.execute("DROP TABLE IF EXISTS webfront_database")
+    cur.execute(
+        """
+        CREATE TABLE webfront_database
+        (
+            name VARCHAR(10) NOT NULL PRIMARY KEY,
+            name_alt VARCHAR(10) NOT NULL,
+            name_long VARCHAR(25) NOT NULL,
+            description LONGTEXT,
+            type ENUM('protein', 'entry', 'feature', 'other') NOT NULL,
+            num_entries INTEGER,
+            version VARCHAR(20),
+            release_date DATETIME,
+            prev_version VARCHAR(20),
+            prev_release_date DATETIME
+        ) CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci
+        """
+    )
+
+    query = """
+        INSERT INTO webfront_database 
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s) 
+    """
+    args = []
+
+    for database in loadobj(databases_file):
+        args.append(database)
+
+    cur.executemany(query, args)
+    con.commit()
+    cur.close()
+    con.close()
+
+    logger.info("done")
+
+
 def insert_entries(ipr_url: str, pfam_url: str, entries_file: str,
                    entry2xrefs_file: str):
     logger.info("fetching Wikipedia data for Pfam entries")
