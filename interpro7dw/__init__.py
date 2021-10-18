3aad0d4eeba7ef8139c81f14341863062df22e98
Matthias Blum <mat.blum@gmail.com>
2021-10-18 18:27:48 +0100

    Move logger in utils package

--- interpro7dw/__init__.py
+++ interpro7dw/__init__.py
@@ -1,15 +1,1 @@
 __version__ = "0.10.4"
-
-import logging
-
-logger = logging.getLogger(__name__)
-if not logger.hasHandlers():
-    logger.setLevel(logging.INFO)
-    _ch = logging.StreamHandler()
-    _ch.setFormatter(
-        logging.Formatter(
-            fmt='%(asctime)s: %(levelname)s: %(message)s',
-            datefmt='%Y-%m-%d %H:%M:%S'
-        )
-    )
-    logger.addHandler(_ch)
