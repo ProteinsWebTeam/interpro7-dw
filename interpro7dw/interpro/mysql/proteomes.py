8335e66c9144667d8e35994f0da0c56e7c862562
Matthias Blum <mat.blum@gmail.com>
2022-02-27 20:24:47 +0000

    Complete insert-proteomes

--- interpro7dw/interpro/mysql/proteomes.py
+++ interpro7dw/interpro/mysql/proteomes.py
@@ -1,22 +1,21 @@
+import pickle
+
 import MySQLdb
 
 from interpro7dw.utils import logger
-from interpro7dw.utils.mysql import url2dict
-from interpro7dw.utils.store import SimpleStore, loadobj
+from interpro7dw.utils.mysql import uri2dict
+from interpro7dw.utils.store import BasicStore
 
 from .utils import jsonify
 
 
-def insert_proteomes(url: str, proteomes_file: str, xrefs_file: str):
-    """
+def populate(uri: str, proteomes_file: str, xrefs_file: str):
+    logger.info("loading proteomes")
+    with open(proteomes_file, "rb") as fh:
+        proteomes = pickle.load(fh)
 
-    :param url:
-    :param proteomes_file:
-    :param xrefs_file:
-    :return:
-    """
     logger.info("creating webfront_proteome")
-    con = MySQLdb.connect(**url2dict(url), charset="utf8mb4")
+    con = MySQLdb.connect(**uri2dict(uri), charset="utf8mb4")
     cur = con.cursor()
     cur.execute("DROP TABLE IF EXISTS webfront_proteome")
     cur.execute(
@@ -37,23 +36,19 @@ def insert_proteomes(url: str, proteomes_file: str, xrefs_file: str):
     query = """
         INSERT INTO webfront_proteome VALUES (%s, %s, %s, %s, %s, %s, %s) 
     """
-    args = []
+    params = []
 
-    proteomes = loadobj(proteomes_file)
-    with SimpleStore(xrefs_file) as store:
+    with BasicStore(xrefs_file, mode="r") as store:
         for proteome_id, xrefs in store:
             proteome = proteomes[proteome_id]
 
             # Adds total number of entries
-            entries = {}
-            total = 0
-            for db, accessions in xrefs["entries"].items():
-                entries[db] = len(accessions)
-                total += entries[db]
+            num_entries = {"total": 0}
+            for database, entries in xrefs["entries"].items():
+                num_entries[database] = len(entries)
+                num_entries["total"] += len(entries)
 
-            entries["total"] = total
-
-            args.append((
+            params.append((
                 proteome_id,
                 proteome["name"],
                 1 if proteome["is_reference"] else 0,
@@ -61,7 +56,7 @@ def insert_proteomes(url: str, proteomes_file: str, xrefs_file: str):
                 proteome["assembly"],
                 proteome["taxon_id"],
                 jsonify({
-                    "domain_architectures": len(xrefs["domain_architectures"]),
+                    "domain_architectures": len(xrefs["dom_orgs"]),
                     "entries": entries,
                     "proteins": xrefs["proteins"],
                     "sets": len(xrefs["sets"]),
@@ -70,13 +65,12 @@ def insert_proteomes(url: str, proteomes_file: str, xrefs_file: str):
                 })
             ))
 
-            if len(args) == 1000:
-                cur.executemany(query, args)
-                args.clear()
+            if len(params) == 1000:
+                cur.executemany(query, params)
+                params = []
 
-    if args:
-        cur.executemany(query, args)
-        args.clear()
+    if params:
+        cur.executemany(query, params)
 
     con.commit()
     cur.close()
