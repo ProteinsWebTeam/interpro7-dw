f238e82fa43c265e39975f740433abd17b3f748d
Matthias Blum <mat.blum@gmail.com>
2022-03-03 12:14:37 +0000

    Functions to export secondary structures and CATH/SCOP domains

--- interpro7dw/pdbe.py
+++ interpro7dw/pdbe.py
@@ -275,6 +275,112 @@ def export_structures(uri: str, output: str):
     logger.info("done")
 
 
+def export_secondary_structures(uri: str, output: str):
+    logger.info("starting")
+    con = cx_Oracle.connect(uri)
+    cur = con.cursor()
+    cur.execute(
+        """
+        SELECT SS.ENTRY_ID, SS.STRUCT_ASYM_ID, SS.ELEMENT_TYPE,
+          R1.UNP_SEQ_ID AS POS_FROM, R1.CHEM_COMP_ID AS RES_FROM,
+          R2.UNP_SEQ_ID AS POS_TO, R2.CHEM_COMP_ID AS RES_TO
+        FROM (
+          SELECT ENTRY_ID, STRUCT_ASYM_ID, ELEMENT_TYPE,
+            RESIDUE_BEG_ID, RESIDUE_END_ID
+          FROM PDBE.SS_HELIX
+          UNION ALL
+          SELECT ENTRY_ID, STRUCT_ASYM_ID, ELEMENT_TYPE,
+            RESIDUE_BEG_ID, RESIDUE_END_ID
+          FROM PDBE.SS_STRAND
+        ) SS
+        INNER JOIN SIFTS_ADMIN.SIFTS_XREF_RESIDUE R1
+          ON (SS.ENTRY_ID=R1.ENTRY_ID
+            AND SS.STRUCT_ASYM_ID=R1.STRUCT_ASYM_ID
+            AND SS.RESIDUE_BEG_ID=R1.ID
+            AND R1.CANONICAL_ACC=1
+            AND R1.OBSERVED='Y'
+            AND R1.UNP_SEQ_ID IS NOT NULL)
+        INNER JOIN SIFTS_ADMIN.SIFTS_XREF_RESIDUE R2
+          ON (SS.ENTRY_ID=R2.ENTRY_ID
+            AND SS.STRUCT_ASYM_ID=R2.STRUCT_ASYM_ID
+            AND SS.RESIDUE_END_ID=R2.ID
+            AND R2.CANONICAL_ACC=1
+            AND R2.OBSERVED='Y'
+            AND R2.UNP_SEQ_ID IS NOT NULL)
+        """
+    )
+
+    structures = {}
+    for row in cur:
+        pdb_id = row[0]
+        try:
+            chains = structures[pdb_id]
+        except KeyError:
+            chains = structures[pdb_id] = {}
+
+        chain_id = row[1]
+        try:
+            chain = chains[chain_id]
+        except KeyError:
+            chain = chains[chain_id] = {}
+
+        elem_type = row[2]
+        try:
+            fragments = chain[elem_type]
+        except KeyError:
+            fragments = chain[elem_type] = []
+
+        fragments.append({
+            # add the type of secondary structure to the fragment
+            "shape": elem_type,
+            "start": row[3],
+            "end": row[5],
+            # "res_start": row[4],
+            # "res_end": row[6],
+        })
+
+    # Sort chains by fragment
+    for pdb_id, chains in structures.items():
+        sorted_chains = []
+
+        for chain_id in sorted(chains):
+            locations = []
+
+            for elem_type, fragments in chains[chain_id].items():
+                fragments.sort(key=lambda f: (f["start"], f["end"]))
+                locations.append({
+                    "fragments": fragments
+                })
+
+            sorted_chains.append({
+                "accession": chain_id,
+                "locations": locations
+            })
+
+        structures[pdb_id] = sorted_chains
+
+    cur.close()
+    con.close()
+
+    with open(output, "wb") as fh:
+        pickle.dump(structures, fh)
+
+    logger.info("done")
+
+
+def export_cath_scop(uri: str, output: str):
+    logger.info("loading CATH domains")
+    cath = get_cath_domains(uri)
+
+    logger.info("loading SCOP domains")
+    scop = get_scop_domains(uri)
+
+    with open(output, "wb") as fh:
+        pickle.dump((cath, scop), fh)
+
+    logger.info("done")
+
+
 def get_cath_domains(uri: str) -> dict:
     con = cx_Oracle.connect(uri)
     cur = con.cursor()
