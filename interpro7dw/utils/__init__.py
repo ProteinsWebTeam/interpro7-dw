3904eeda38b85f27856fc5d18965383d788236a6
Matthias Blum <mat.blum@gmail.com>
2022-03-06 20:04:32 +0000

    Adding ProgressTracker class

--- interpro7dw/utils/__init__.py
+++ interpro7dw/utils/__init__.py
@@ -1,4 +1,5 @@
 import logging
+import math
 
 
 logger = logging.getLogger(__name__)
@@ -12,3 +13,24 @@ if not logger.hasHandlers():
         )
     )
     logger.addHandler(_ch)
+
+
+class ProgressTracker:
+    def __init__(self, total: int, step: float):
+        self.i = 0
+        self.total = total
+        self.step = self.milestone = math.ceil(total * step)
+        self.pc_step = self.pc_milestone = step * 100
+
+    def incr(self):
+        self.i += 1
+
+        if self.i == self.milestone:
+            logger.info(f"{self.pc_milestone:>10.0f}%")
+            self.milestone += self.step
+
+            if self.milestone <= self.total:
+                self.pc_milestone += self.pc_step
+            else:
+                self.milestone = self.total
+                self.pc_milestone = 100
