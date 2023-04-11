d8cb1a2fcba23943003757ebb47ae0b0705eb3ad
Laise Florentino <laisecflorentino@gmail.com>
2023-04-11 14:51:51 +0100

    update export alphafold
    Updating code to not add alphafold with sequence change

--- interpro7dw/alphafold.py
+++ interpro7dw/alphafold.py
@@ -7,7 +7,6 @@ from interpro7dw.utils.store import KVStore, KVStoreBuilder
 def export(alphafold_file: str, proteins_file: str, output: str,
            keep_fragments: bool = False, tempdir: Optional[str] = None):
     """Export proteins with AlphaFold predictions.
-
     :param alphafold_file: TSV file of AlphaFold predictions.
     :param proteins_file: File to KVStore of proteins.
     :param output: Output KVStore file.
@@ -21,26 +20,35 @@ def export(alphafold_file: str, proteins_file: str, output: str,
     with KVStore(proteins_file) as st:
         keys = st.get_keys()
 
-    with KVStoreBuilder(output, keys=keys, tempdir=tempdir) as ash:
-        with open(alphafold_file, "rt") as fh:
-            for line in fh:
-                """
-                Columns:
-                    - UniProt accession, e.g. A8H2R3
-                    - AlphaFold DB identifier, e.g. AF-A8H2R3-F1
-                    - mean pLDDT of the prediction
-                """
-                cols = line.rstrip().split()
-                uniprot_acc = cols[0]
-                alphafold_id = cols[1]
-                score = float(cols[2])
-                ash.add(uniprot_acc, (alphafold_id, score))
+    with KVStore(proteins_file) as protein:
+        with KVStoreBuilder(output, keys=keys, tempdir=tempdir) as ash:
+            with open(alphafold_file, "rt") as fh:
+                for line in fh:
+                    """
+                    Columns:
+                        - UniProt accession, e.g. A8H2R3
+                        - AlphaFold DB identifier, e.g. AF-A8H2R3-F1
+                        - mean pLDDT of the prediction
+                        - sequence crc64 hash
+                    """
+                    cols = line.rstrip().split()
+                    uniprot_acc = cols[0]
+                    alphafold_id = cols[1]
+                    score = float(cols[2])
+                    crc64 = cols[3]
+                    try:
+                        protein_info = protein[uniprot_acc]
+                    except KeyError:
+                        continue
+                    else:
+                        if protein_info["crc64"] == crc64:
+                            ash.add(uniprot_acc, (alphafold_id, score))
 
-        if keep_fragments:
-            ash.build(apply=lambda x: x)
-        else:
-            ash.build(apply=lambda x: x if len(x) == 1 else [])
+            if keep_fragments:
+                ash.build(apply=lambda x: x)
+            else:
+                ash.build(apply=lambda x: x if len(x) == 1 else [])
 
-        logger.info(f"temporary files: {ash.get_size() / 1024 ** 2:.0f} MB")
+            logger.info(f"temporary files: {ash.get_size() / 1024 ** 2:.0f} MB")
 
     logger.info("done")
