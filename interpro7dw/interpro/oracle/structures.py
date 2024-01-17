9ca96a59d2bbce4bc7e1dcca2d60e3e21c3e5b5f
Matthias Blum <mat.blum@gmail.com>
2024-01-17 13:59:23 +0000

    Only export PDB matches for structures with SIFTS mappings

--- interpro7dw/interpro/oracle/structures.py
+++ interpro7dw/interpro/oracle/structures.py
@@ -2,11 +2,13 @@ import glob
 import gzip
 import os
 import shelve
+from tempfile import mkstemp
 
 import oracledb
 
 from interpro7dw.pdbe import get_sequences, iter_sifts_mappings
 from interpro7dw.utils import logger
+from interpro7dw.utils.store import BasicStore
 from interpro7dw.utils.oracle import lob_as_str
 from .databases import get_databases_codes
 from .entries import load_entries, load_signatures
@@ -40,17 +42,16 @@ def update_pdbe_matches(uri: str):
     logger.info(f"{cnt:,} rows inserted")
 
 
-def export_matches(ipr_uri: str, pdbe_uri: str, output: str):
-    logger.info("starting")
-    for file in glob.glob(f"{output}*"):
-        os.unlink(file)
+def export_matches(ipr_uri: str, pdbe_uri: str, output: str,
+                   tempdir: str | None = None):
+    fd, sequences_file = mkstemp(dir=tempdir)
+    os.close(fd)
 
-    con = oracledb.connect(ipr_uri)
-    cur = con.cursor()
-    cur.outputtypehandler = lob_as_str
-
-    with shelve.open(output, writeback=True) as db:
+    with BasicStore(sequences_file, mode="w") as bs:
         logger.info("exporting sequences from UniParc")
+        con = oracledb.connect(ipr_uri)
+        cur = con.cursor()
+        cur.outputtypehandler = lob_as_str
         cur.execute(
             """
             SELECT X.AC, P.SEQ_SHORT, P.SEQ_LONG
@@ -61,54 +62,77 @@ def export_matches(ipr_uri: str, pdbe_uri: str, output: str):
             """
         )
 
-        i = 0
         chains = set()
         for pdb_chain, seq_short, seq_long in cur:
             sequence = seq_short or seq_long
             chains.add(pdb_chain)
-            db[pdb_chain] = {
-                "length": len(sequence),
-                "sequence": gzip.compress(sequence.encode("utf-8")),
-                "matches": [],
-                "sifts": {}
-            }
+            bs.write((
+                pdb_chain,
+                len(sequence),
+                gzip.compress(sequence.encode("utf-8"))
+            ))
 
-            i += 1
-            if i % 1000 == 0:
-                db.sync()
-
-        db.sync()
+        cur.close()
+        con.close()
 
         logger.info("exporting sequences from PDBe")
-        i = 0
         for pdb_chain, sequence in get_sequences(pdbe_uri):
             if pdb_chain not in chains:
                 chains.add(pdb_chain)
-                db[pdb_chain] = {
-                    "length": len(sequence),
-                    "sequence": gzip.compress(sequence.encode("utf-8")),
-                    "matches": [],
-                    "sifts": {}
-                }
+                bs.write((
+                    pdb_chain,
+                    len(sequence),
+                    gzip.compress(sequence.encode("utf-8"))
+                ))
 
-                i += 1
-                if i % 1000 == 0:
-                    db.sync()
+    fd, sifts_file = mkstemp(dir=tempdir)
+    os.close(fd)
 
-        db.sync()
-
-        logger.info("exporting SIFTS mappings")
-        i = 0
+    logger.info("exporting SIFTS mappings")
+    sifts_chains = set()
+    with BasicStore(sifts_file, mode="w") as bs:
         for pdb_chain, residues in iter_sifts_mappings(pdbe_uri, chains):
-            db[pdb_chain]["sifts"] = residues
+            bs.write((pdb_chain, residues))
+            sifts_chains.add(pdb_chain)
 
-            i += 1
-            if i % 1000 == 0:
-                db.sync()
+    for file in glob.glob(f"{output}*"):
+        os.unlink(file)
+
+    with shelve.open(output, writeback=True) as db:
+        logger.info("shelving sequences")
+        i = 0
+        chains.clear()
+        with BasicStore(sequences_file, mode="r") as bs:
+            for pdb_chain, length, sequence in bs:
+                if pdb_chain in sifts_chains:
+                    chains.add(pdb_chain)
+                    db[pdb_chain] = {
+                        "length": length,
+                        "sequence": sequence,
+                        "matches": [],
+                        "sifts": {}
+                    }
+                    i += 1
+                    if i % 1000 == 0:
+                        db.sync()
 
         db.sync()
 
-        logger.info("exporting matches")
+        logger.info("shelving SIFTS mappings")
+        i = 0
+        with BasicStore(sifts_file, mode="r") as bs:
+            for pdb_chain, residues in bs:
+                if pdb_chain in chains:
+                    db[pdb_chain]["sifts"] = residues
+                    i += 1
+                    if i % 1000 == 0:
+                        db.sync()
+
+        db.sync()
+
+        logger.info("shelving matches")
+        con = oracledb.connect(ipr_uri)
+        cur = con.cursor()
         dbcodes, _ = get_databases_codes(cur)
         params = [f":{i+1}" for i in range(len(dbcodes))]
         cur.execute(
@@ -131,30 +155,26 @@ def export_matches(ipr_uri: str, pdbe_uri: str, output: str):
 
         i = 0
         for pdb_chain, signature_acc, pos_start, pos_end, fragments in cur:
-            db[pdb_chain]["matches"].append((
-                signature_acc,
-                None,  # model accession
-                None,  # feature
-                None,  # score
-                get_fragments(pos_start, pos_end, fragments)
-            ))
+            if pdb_chain in chains:
+                db[pdb_chain]["matches"].append((
+                    signature_acc,
+                    None,  # model accession
+                    None,  # feature
+                    None,  # score
+                    get_fragments(pos_start, pos_end, fragments)
+                ))
 
-            i += 1
-            if i % 1e3 == 0:
-                db.sync()
-
-            if i % 1e6 == 0:
-                logger.info(f"{i:>15,}")
+                i += 1
+                if i % 1000 == 0:
+                    db.sync()
 
         db.sync()
-        logger.info(f"{i:>15,}")
 
-    logger.info("loading entries")
-    entries = load_entries(cur)
-    signatures = load_signatures(cur)
-
-    cur.close()
-    con.close()
+        logger.info("loading entries")
+        entries = load_entries(cur)
+        signatures = load_signatures(cur)
+        cur.close()
+        con.close()
 
     logger.info("merging matches")
     with shelve.open(output, writeback=False) as db:
@@ -163,4 +183,10 @@ def export_matches(ipr_uri: str, pdbe_uri: str, output: str):
             obj["matches"] = {**s, **e}
             db[pdb_chain] = obj
 
+    size = 0
+    for file in [sequences_file, sifts_file]:
+        size += os.path.getsize(file)
+        os.unlink(file)
+
+    logger.info(f"temporary files: {size / 1024 ** 2:.0f} MB")
     logger.info("done")
