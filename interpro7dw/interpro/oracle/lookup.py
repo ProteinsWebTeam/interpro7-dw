0e362e1dfa2d29743c25f8b16a6424631e3a8739
Matthias Blum <mat.blum@gmail.com>
2024-09-02 15:25:21 +0100

    Remove additional bind param

--- interpro7dw/interpro/oracle/lookup.py
+++ interpro7dw/interpro/oracle/lookup.py
@@ -263,7 +263,7 @@ def _insert_matches(cur: oracledb.Cursor, file: str, batchsize: int = 10000):
             """
             INSERT /*+ APPEND */ INTO IPRSCAN.LOOKUP_MATCH
             VALUES (:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, 
-                    :12, :13, :14, :15, :16, :17, :18, :19, :20)
+                    :12, :13, :14, :15, :16, :17, :18, :19)
             """,
             records[i:i+batchsize]
         )
