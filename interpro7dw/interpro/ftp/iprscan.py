3ceb8fb86e591a06f28faf7d6080136e330154f8
Matthias Blum <mat.blum@gmail.com>
2024-09-17 14:46:19 +0100

    Consistency in prefix

--- interpro7dw/interpro/ftp/iprscan.py
+++ interpro7dw/interpro/ftp/iprscan.py
@@ -41,7 +41,7 @@ def package_data(ipr_uri: str, goa_uri: str, data_dir: str, ipr_version: str,
 
     data_dir = Path(data_dir)
     output = Path(outdir) / "interproscan-data.tar.gz"
-    prefix = f"interproscan-{ipr_version}/"
+    prefix = f"interproscan-data-{ipr_version}/"
     with tarfile.open(str(output), "w:gz") as tar:
         logger.info("Archiving JSON files")
         for file in [pathways_file, entry2pathways_file, terms_file,
