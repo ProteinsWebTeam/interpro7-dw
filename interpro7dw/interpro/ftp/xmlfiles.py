5b752d8a0d0c35d3869e99586240a5a6e75dfb36
apolignano <alessandro.polignano@ebi.ac.uk>
2024-11-14 14:58:05 +0000

    Add status and tax id attributes to protein element

--- interpro7dw/interpro/ftp/xmlfiles.py
+++ interpro7dw/interpro/ftp/xmlfiles.py
@@ -477,6 +477,8 @@ def _export_matches(proteins_file: str, matches_file: str,
                 elem.setAttribute("name", protein["identifier"])
                 elem.setAttribute("length", str(protein["length"]))
                 elem.setAttribute("crc64", protein["crc64"])
+                elem.setAttribute("status", protein["status"])
+                elem.setAttribute("taxid", protein["taxid"])
 
                 signatures, entries = st2.get(protein_acc, ({}, {}))
                 for signature_acc in sorted(signatures):
