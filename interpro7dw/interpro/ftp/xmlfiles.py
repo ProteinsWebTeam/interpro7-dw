75a89580e396d48d4d40f6031fc2d570601ad6a8
apolignano <alessandro.polignano@ebi.ac.uk>
2024-11-21 15:32:14 +0000

    print log

--- interpro7dw/interpro/ftp/xmlfiles.py
+++ interpro7dw/interpro/ftp/xmlfiles.py
@@ -486,6 +486,7 @@ def _export_matches(proteins_file: str, matches_file: str, features_file: str,
                 elem.setAttribute("crc64", protein["crc64"])
 
                 for kv_store in [st2, ff]:
+                    print(kv_store.get(protein_acc, ({}, {})))
                     signatures, entries = kv_store.get(protein_acc, ({}, {}))
                     for signature_acc in sorted(signatures):
                         signature = signatures[signature_acc]
