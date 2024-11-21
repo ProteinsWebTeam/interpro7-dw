b5d0158ea129f9dc189c1e51823faa0f0085261c
Matthias Blum <mat.blum@gmail.com>
2024-11-21 16:40:25 +0000

    Create and ingest SST files

--- interpro7dw/interpro/lookup.py
+++ interpro7dw/interpro/lookup.py
@@ -1,14 +1,16 @@
+import glob
 import json
 import os
 import shutil
+from concurrent.futures import ProcessPoolExecutor, as_completed
 
-import rocksdict
+from rocksdict import Rdict, Options, SstFileWriter
 
 from interpro7dw.utils import logger
 from interpro7dw.utils.store import BasicStore
 
 
-def build(indir: str, outdir: str):
+def build(indir: str, outdir: str, processes: int = 8):
     logger.info("starting")
 
     try:
@@ -18,7 +20,7 @@ def build(indir: str, outdir: str):
 
     os.makedirs(outdir, mode=0o775)
 
-    opt = rocksdict.Options(raw_mode=True)
+    opt = Options(raw_mode=True)
     # Increase the size of the write buffer (default: 64MB)
     opt.set_write_buffer_size(256 * 1024 * 1024)
     opt.set_level_zero_file_num_compaction_trigger(4)
@@ -34,39 +36,60 @@ def build(indir: str, outdir: str):
     # https://github.com/facebook/rocksdb/wiki/RocksDB-FAQ#basic-readwrite
     opt.prepare_for_bulk_load()
 
-    db = rocksdict.Rdict(outdir, options=opt)
+    db = Rdict(outdir, options=opt)
 
-    files = []
-    for filename in sorted(os.listdir(indir)):
-        filepath = os.path.join(indir, filename)
-        files.append(filepath)
+    errors = 0
+    with ProcessPoolExecutor(max_workers=max(1, processes - 1)) as executor:
+        fs = []
+        for filepath in glob.glob(os.path.join(indir, "*.dat")):
+            f = executor.submit(create_sst, filepath)
+            fs.append(f)
 
-    milestone = step = 5
-    for i, filepath in enumerate(files):
-        with BasicStore(filepath) as bs:
-            for proteins in bs:
-                wb = rocksdict.WriteBatch(raw_mode=True)
-                for protein in proteins.values():
-                    md5 = protein["md5"]
-                    matches = protein["matches"]
+        milestone = step = 5
+        for i, f in enumerate(as_completed(fs)):
+            try:
+                paths = f.result()
+            except Exception as exc:
+                logger.error(exc)
+                errors += 1
+            else:
+                db.ingest_external_file(paths)
 
-                    # Remove extra fields
-                    for match in matches:
-                        del match["extra"]
-                        for loc in match["locations"]:
-                            del loc["extra"]
+            progress = (i + 1) * 100 / len(fs)
+            if progress >= milestone:
+                logger.info(f"{progress:.0f}%")
+                milestone += step
 
-                    wb.put(md5.encode("utf-8"),
-                           json.dumps(matches).encode("utf-8"))
-
-                db.write(wb)
-
-        progress = (i + 1) * 100 / len(files)
-        if progress >= milestone:
-            logger.info(f"{progress:.0f}%")
-            milestone += step
+    if errors:
+        db.close()
+        raise RuntimeError(f"{errors} occurred")
 
     logger.info("compacting")
     db.compact_range(None, None)
     db.close()
     logger.info("done")
+
+
+def create_sst(filepath: str) -> list[str]:
+    paths = []
+    with BasicStore(filepath, mode="r", compresslevel=0) as bs:
+        for i, proteins in enumerate(bs):
+            sstfile = f"{filepath}.{i}.sst"
+            writer = SstFileWriter(Options(raw_mode=True))
+            writer.open(sstfile)
+
+            for p in sorted(proteins.values(), key=lambda x: x["md5"]):
+                # Remove extra fields
+                for match in p["matches"]:
+                    del match["extra"]
+                    for loc in match["locations"]:
+                        del loc["extra"]
+
+                key = p["md5"].encode("utf-8")
+                value = json.dumps(p["matches"]).encode("utf-8")
+                writer[key] = value
+
+            writer.finish()
+            paths.append(sstfile)
+
+    return paths
