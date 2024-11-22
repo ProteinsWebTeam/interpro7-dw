056656d57e3965fbaa68d1a9af1155e7e0b58ec5
Matthias Blum <mat.blum@gmail.com>
2024-11-22 11:41:02 +0000

    Fix multi-line string

--- interpro7dw/interpro/oracle/lookup.py
+++ interpro7dw/interpro/oracle/lookup.py
@@ -142,7 +142,7 @@ def create_matches_table(uri: str, indir: str, processes: int = 8):
 def insert_matches(uri: str, inqueue: Queue, outqueue: Queue):
     con = oracledb.connect(uri)
     cur = con.cursor()
-    statement = """"
+    statement = """
     INSERT /*+ APPEND */ INTO IPRSCAN.LOOKUP_MATCH
     VALUES (:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, 
             :12, :13, :14, :15, :16, :17, :18, :19, :20)
