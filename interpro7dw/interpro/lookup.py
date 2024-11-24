2a20ccc9b670327430aa623220ccfc3c077e6b16
Matthias Blum <mat.blum@gmail.com>
2024-11-24 17:38:32 +0000

    Creat bigger SST files

--- interpro7dw/interpro/lookup.py
+++ interpro7dw/interpro/lookup.py
@@ -1,8 +1,10 @@
 import glob
+import heapq
 import json
 import os
 import shutil
 from concurrent.futures import ProcessPoolExecutor, as_completed
+from tempfile import mkstemp
 
 from rocksdict import Rdict, Options, SstFileWriter
 
@@ -10,7 +12,8 @@ from interpro7dw.utils import logger
 from interpro7dw.utils.store import BasicStore
 
 
-def build(indir: str, outdir: str, processes: int = 8):
+def build(indir: str, outdir: str, processes: int = 8,
+          tempdir: str | None = None):
     logger.info("starting")
 
     try:
@@ -19,6 +22,9 @@ def build(indir: str, outdir: str, processes: int = 8):
         pass
 
     os.makedirs(outdir, mode=0o775)
+
+    if tempdir:
+        os.makedirs(tempdir, exist_ok=True)
 
     opt = Options(raw_mode=True)
     # Increase the size of the write buffer (default: 64MB)
@@ -42,20 +48,18 @@ def build(indir: str, outdir: str, processes: int = 8):
     with ProcessPoolExecutor(max_workers=max(1, processes - 1)) as executor:
         fs = []
         for filepath in glob.glob(os.path.join(indir, "*.dat")):
-            f = executor.submit(create_sst, filepath)
+            f = executor.submit(create_sst, filepath, tempdir)
             fs.append(f)
 
         milestone = step = 5
         for i, f in enumerate(as_completed(fs)):
             try:
-                paths = f.result()
+                path = f.result()
             except Exception as exc:
                 logger.error(exc)
                 errors += 1
             else:
-                db.ingest_external_file(paths)
-
-            for path in paths:
+                db.ingest_external_file([path])
                 os.unlink(path)
 
             progress = (i + 1) * 100 / len(fs)
@@ -73,14 +77,14 @@ def build(indir: str, outdir: str, processes: int = 8):
     logger.info("done")
 
 
-def create_sst(filepath: str) -> list[str]:
-    paths = []
+def create_sst(filepath: str, tempdir: str | None = None) -> str:
+    stores = []
     with BasicStore(filepath, mode="r", compresslevel=0) as bs:
         for i, proteins in enumerate(bs):
-            sstfile = f"{filepath}.{i}.sst"
-            writer = SstFileWriter(Options(raw_mode=True))
-            writer.open(sstfile)
+            fd, temppath = mkstemp(dir=tempdir)
+            os.close(fd)
 
+            bs2 = BasicStore(temppath, mode="w", compresslevel=0)
             for p in sorted(proteins.values(), key=lambda x: x["md5"]):
                 # Remove extra fields
                 for match in p["matches"]:
@@ -88,11 +92,23 @@ def create_sst(filepath: str) -> list[str]:
                     for loc in match["locations"]:
                         del loc["extra"]
 
-                key = p["md5"].encode("utf-8")
-                value = json.dumps(p["matches"]).encode("utf-8")
-                writer[key] = value
+                bs2.write((p["md5"], p["matches"]))
 
-            writer.finish()
-            paths.append(sstfile)
+            bs2.close()
+            stores.append(bs2)
 
-    return paths
+    sstfile = f"{filepath}.sst"
+    writer = SstFileWriter(Options(raw_mode=True))
+    writer.open(sstfile)
+    iterable = [iter(bs) for bs in stores]
+    for md5, matches in heapq.merge(*iterable, key=lambda x: x[0]):
+        key = md5.encode("utf-8")
+        value = json.dumps(matches).encode("utf-8")
+        writer[key] = value
+
+    writer.finish()
+
+    for bs in stores:
+        os.unlink(bs.file)
+
+    return sstfile
