147e670b3d2eac8715bbd1a2d28fac5741ef99f8
Matthias Blum <mat.blum@gmail.com>
2025-04-14 11:21:55 +0100

    Add logging messages

--- interpro7dw/interpro/ftp/lookup.py
+++ interpro7dw/interpro/ftp/lookup.py
@@ -1,13 +1,18 @@
 import os
 import tarfile
 
+from interpro7dw.utils import logger
+
 _LOOKUP_TAR = "matches-api-data.tar.gz"
 
 
 def archive(indir: str, version: str, outdir: str):
+    logger.info("starting")
     prefix = f"interpro-{version}"
     lookup_dir = os.path.join(indir, version)
     with tarfile.open(os.path.join(outdir, _LOOKUP_TAR), "w:gz") as fh:
         for filename in os.listdir(lookup_dir):
             filepath = os.path.join(lookup_dir, filename)
             fh.add(filepath, arcname=os.path.join(prefix, filename))
+
+    logger.info("done")
