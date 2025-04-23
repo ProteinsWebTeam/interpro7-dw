614d11a64f666a79db896c8f63bd7b3dee60decf
Matthias Blum <mat.blum@gmail.com>
2025-04-23 09:35:05 +0100

    Update copyright notice

--- interpro7dw/interpro/ftp/xmlfiles.py
+++ interpro7dw/interpro/ftp/xmlfiles.py
@@ -692,8 +692,19 @@ def export_toad_matches(
         fh.write(
             f"""\
 <!--
-    Copyright (c) {date.today():%Y} Google DeepMind
-    Licensed under CC BY 4.0 (see https://creativecommons.org/licenses/by/4.0/ for full details)
+    Copyright {date.today():%Y} Google LLC
+
+    The materials are licensed under the Creative Commons Attribution 4.0 \
+International License (CC-BY). You may obtain a copy of the CC-BY license at: \
+https://creativecommons.org/licenses/by/4.0/legalcode
+
+    Unless required by applicable law or agreed to in writing, \
+all materials distributed here under the CC-BY licenses are distributed \
+on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, \
+either express or implied. See the licenses for the specific language \
+governing permissions and limitations under those licenses.
+
+    This is not an official Google product.
 -->"""
         )
         fh.write("<interpromatch>\n")
